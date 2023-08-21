3e010c2cfbf42bc66b32a539aa3120b77a579db5
canterbery <wotphantommail@gmail.com>
2023-08-21 16:06:43 +0300

    wr-20: + notification message enum

---
+++ frontend/src/packages/notification/libs/enums/notification-message.enum.ts
@@ -0,0 +1,8 @@
+const NotificationMessage = {
+  DEFAULT_ERROR_MESSAGE: 'Unknown error!',
+  DEFAULT_SUCCESS_MESSAGE: 'Success!',
+  DEFAULT_WARNING_MESSAGE: 'Warning!',
+  DEFAULT_INFO_MESSAGE: 'Info!',
+};
+
+export { NotificationMessage };
