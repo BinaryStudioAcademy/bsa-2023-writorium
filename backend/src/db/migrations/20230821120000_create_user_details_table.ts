86ac044515b94bc730ea9cecdddbde6e77c5a261
Anna Kovalchuk <annazukh@gmail.com>
2023-08-21 17:27:40 +0300

    wr-7: + user details migration

---
+++ backend/src/db/migrations/20230821120000_create_user_details_table.ts
@@ -0,0 +1,36 @@
+import { type Knex } from 'knex';
+
+const TABLE_NAME = 'user_details';
+const ColumnName = {
+  FIRST_NAME: 'first_name',
+  LAST_NAME: 'last_name',
+  USER_ID: 'user_id',
+  AVATAR_ID: 'avatar_id',
+};
+
+const up = (knex: Knex): Promise<void> => {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.increments();
+    table.text(ColumnName.FIRST_NAME).notNullable();
+    table.text(ColumnName.LAST_NAME).notNullable();
+    table
+      .integer(ColumnName.USER_ID)
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .index();
+    table
+      .integer(ColumnName.AVATAR_ID)
+      .unsigned()
+      .nullable()
+      .references('id')
+      .inTable('files');
+  });
+};
+
+const down = (knex: Knex): Promise<void> => {
+  return knex.schema.dropTableIfExists(TABLE_NAME);
+};
+
+export { down, up };
