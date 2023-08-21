6ae45ed034d2860e05537b4f63814e53fb47f00b
drDramen <vvpalchyn@gmail.com>
2023-08-21 18:18:17 +0300

    wr-16: + add className property, default styles to Link component

--- frontend/src/libs/components/link/link.tsx
+++ frontend/src/libs/components/link/link.tsx
@@ -1,15 +1,21 @@
 import { NavLink } from 'react-router-dom';
 
 import { type AppRoute } from '~/libs/enums/enums.js';
+import { getValidClassNames } from '~/libs/helpers/helpers.js';
 import { type ValueOf } from '~/libs/types/types.js';
+
+import styles from './styles.module.scss';
 
 type Properties = {
   to: ValueOf<typeof AppRoute>;
+  className?: string | undefined;
   children: React.ReactNode;
 };
 
-const Link: React.FC<Properties> = ({ children, to }) => (
-  <NavLink to={to}>{children}</NavLink>
+const Link: React.FC<Properties> = ({ children, className, to }) => (
+  <NavLink to={to} className={getValidClassNames(styles.link, className)}>
+    {children}
+  </NavLink>
 );
 
 export { Link };
