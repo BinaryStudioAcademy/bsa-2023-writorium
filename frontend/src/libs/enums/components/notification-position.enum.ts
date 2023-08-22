21b478c1ad8c68fec0b20d345ac37a0961cbc0a2
canterbery <wotphantommail@gmail.com>
2023-08-22 13:21:38 +0300

    wr-20: + notification position enum

---
+++ frontend/src/libs/enums/components/notification-position.enum.ts
@@ -0,0 +1,8 @@
+const NotificationPosition = {
+  TOP_RIGHT: 'top-right',
+  TOP_LEFT: 'top-left',
+  BOTTOM_LEFT: 'bottom-left',
+  BOTTOM_RIGHT: 'bottom-right',
+} as const;
+
+export { NotificationPosition };
