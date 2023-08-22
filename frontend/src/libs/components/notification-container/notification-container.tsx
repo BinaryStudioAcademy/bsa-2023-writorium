f60dcbafff5fe41b1072d9aee2d362eb9b26b61e
canterbery <wotphantommail@gmail.com>
2023-08-22 11:34:04 +0300

    wr-20: * component name

--- frontend/src/libs/components/notification-container/notification-container.tsx
+++ frontend/src/libs/components/notification-container/notification-container.tsx
@@ -7,11 +7,11 @@ type Properties = {
   position?: 'top-right' | 'top-left' | 'bottom-left' | 'bottom-right';
 };
 
-const NotificationContainer: React.FC<Properties> = ({
+const Notification: React.FC<Properties> = ({
   theme = 'colored',
   position = 'top-right',
 }) => {
   return <ToastContainer theme={theme} position={position} draggable={false} />;
 };
 
-export { NotificationContainer };
+export { Notification };
