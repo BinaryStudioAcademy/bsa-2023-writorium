152096f6494d7bf3dd0ec9583352937a15e96ff4
canterbery <wotphantommail@gmail.com>
2023-08-22 16:01:35 +0300

    wr-20: * action name

--- frontend/src/slices/app/actions.ts
+++ frontend/src/slices/app/actions.ts
@@ -3,10 +3,10 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { type AsyncThunkConfig } from '~/libs/types/types.js';
 import { type NotificationPayload } from '~/packages/notification/notification.js';
 
-import { AppActionType } from './common.js';
+import { name as sliceName } from './app.slice.js';
 
 const notify = createAsyncThunk<unknown, NotificationPayload, AsyncThunkConfig>(
-  AppActionType.NOTIFY,
+  `${sliceName}/notify`,
   (payload, { extra }) => {
     const { notification } = extra;
     const { type, message } = payload;
