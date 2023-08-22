ae96baec4d907b3d9220829e01d87a0951f30e09
Andrew <bobrovskyandrew@gmail.com>
2023-08-22 20:43:42 +0300

    wr-6: + add Message component import

--- frontend/src/libs/components/components.ts
+++ frontend/src/libs/components/components.ts
@@ -2,6 +2,7 @@ export { App } from './app/app.js';
 export { Button } from './button/button.js';
 export { Input } from './input/input.js';
 export { Link } from './link/link.js';
+export { Message } from './message/message.js';
 export { RouterProvider } from './router-provider/router-provider.jsx';
 export { Provider as StoreProvider } from 'react-redux';
 export { Outlet as RouterOutlet } from 'react-router-dom';
