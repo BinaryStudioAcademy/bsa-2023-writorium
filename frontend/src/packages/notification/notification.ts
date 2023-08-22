f13286864dbf7a283b1c4e092999fa4ad491813a
canterbery <wotphantommail@gmail.com>
2023-08-22 15:39:50 +0300

    wr-20: * export paths

--- frontend/src/packages/notification/notification.ts
+++ frontend/src/packages/notification/notification.ts
@@ -2,5 +2,5 @@ import { Notification } from './notification.package.js';
 
 const notification = new Notification();
 
-export { type NotificationPayload } from './libs/types/notification-payload.type.js';
+export { type NotificationPayload } from './libs/types/types.js';
 export { notification };
