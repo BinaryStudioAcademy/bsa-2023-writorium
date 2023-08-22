536ac506aa50ee35f76786ecf04b913cf8e95462
drDramen <vvpalchyn@gmail.com>
2023-08-22 12:51:39 +0300

    wr-16: + implement getAxisOffset helper

--- frontend/src/libs/helpers/helpers.ts
+++ frontend/src/libs/helpers/helpers.ts
@@ -1,2 +1,3 @@
+export { getAxisOffset } from './get-axis-offset/get-axis-offset.js';
 export { getValidClassNames } from './get-valid-class-names/get-valid-class-names.js';
 export { configureString } from 'shared/build/index.js';
