78693dc572b1a5851d42ca37f8f31deb04eed901
Andrew <bobrovskyandrew@gmail.com>
2023-08-22 15:13:39 +0300

    wr-6: + add className property, default styles to Button component

--- frontend/src/libs/components/button/button.tsx
+++ frontend/src/libs/components/button/button.tsx
@@ -1,10 +1,14 @@
+import styles from '~/libs/components/button/styles.module.scss';
+import { getValidClassNames } from '~/libs/helpers/helpers.js';
+
 type Properties = {
   label: string;
   type?: 'button' | 'submit';
+  className?: string | undefined;
 };
 
-const Button: React.FC<Properties> = ({ type = 'button', label }) => (
-  <button type={type}>{label}</button>
+const Button: React.FC<Properties> = ({ type = 'button', label, className }) => (
+  <button className={getValidClassNames(styles.btn, className)} type={type}>{label}</button>
 );
 
 export { Button };
