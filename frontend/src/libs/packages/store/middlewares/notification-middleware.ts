8572b492589ab842b8c1ed6527ccdfe26a288755
canterbery <wotphantommail@gmail.com>
2023-08-22 13:00:27 +0300

    wr-20: - isFulfilled matcher

--- frontend/src/libs/packages/store/middlewares/notification-middleware.ts
+++ frontend/src/libs/packages/store/middlewares/notification-middleware.ts
@@ -1,12 +1,7 @@
 import { type TypedStartListening } from '@reduxjs/toolkit';
-import {
-  createListenerMiddleware,
-  isFulfilled,
-  isRejected,
-} from '@reduxjs/toolkit';
+import { createListenerMiddleware, isRejected } from '@reduxjs/toolkit';
 
 import { type AsyncThunkConfig } from '~/libs/types/async-thunk-config.type';
-import { NotificationMessage } from '~/packages/notification/libs/enums/notification-message.enum';
 import { NotificationType } from '~/packages/notification/libs/enums/notification-type.enum';
 import { appActions } from '~/slices/app/app.js';
 
@@ -23,36 +18,13 @@ const startAppListening =
 startAppListening({
   matcher: isRejected,
   effect: (action, listenerApi) => {
-    const config = NotificationMessage[action.type];
-    if (config) {
-      void listenerApi.dispatch(
-        appActions.notify({
-          type: config.type,
-          message: config.message,
-        }),
-      );
-    } else {
-      void listenerApi.dispatch(
-        appActions.notify({
-          type: NotificationType.ERROR,
-        }),
-      );
-    }
-  },
-});
-
-startAppListening({
-  matcher: isFulfilled,
-  effect: (action, listenerApi) => {
-    const config = NotificationMessage[action.type];
-    if (config) {
-      void listenerApi.dispatch(
-        appActions.notify({
-          type: config.type,
-          message: config.message,
-        }),
-      );
-    }
+    const errorMessage = action.error.message ?? 'Unknown error!';
+    void listenerApi.dispatch(
+      appActions.notify({
+        type: NotificationType.ERROR,
+        message: errorMessage,
+      }),
+    );
   },
 });
 
