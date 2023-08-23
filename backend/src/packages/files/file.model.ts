dd31ab80badc501a103d5b2e81ce80d53d15abd7
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-08-23 23:15:49 +0300

    wr-42: + file model implementation

--- backend/src/packages/files/file.model.ts
+++ backend/src/packages/files/file.model.ts
@@ -1,5 +1,14 @@
-import { AbstractModel } from '~/libs/packages/database/database.js';
+import {
+  AbstractModel,
+  DatabaseTableName,
+} from '~/libs/packages/database/database.js';
 
-class FileModel extends AbstractModel {}
+class FileModel extends AbstractModel {
+  public 'url': string;
+
+  public static override get tableName(): string {
+    return DatabaseTableName.FILES;
+  }
+}
 
 export { FileModel };
