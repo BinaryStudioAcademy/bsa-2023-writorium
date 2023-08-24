44b47d0c02e2d482e241b05f0a2acb500fc146a2
drDramen <vvpalchyn@gmail.com>
2023-08-25 01:07:49 +0300

    wr-31: + implement article service

---
+++ backend/src/packages/articles/article.service.ts
@@ -0,0 +1,83 @@
+import { ApplicationError } from '~/libs/exceptions/exceptions.js';
+import { type IService } from '~/libs/interfaces/service.interface.js';
+
+import { ArticleEntity } from './article.entity.js';
+import { type ArticleRepository } from './article.repository.js';
+import {
+  type ArticleCreateDto,
+  type ArticleCreateResponseDto,
+  type ArticleGetItemResponseDto,
+  type ArticleUpdateRequestDto,
+  type ArticleUpdateResponseDto,
+} from './libs/types/types.js';
+
+class ArticleService implements IService {
+  private articleRepository: ArticleRepository;
+
+  public constructor(articleRepository: ArticleRepository) {
+    this.articleRepository = articleRepository;
+  }
+
+  public find(): Promise<unknown> {
+    return Promise.resolve();
+  }
+
+  public findAll(): Promise<{ items: unknown[] }> {
+    return Promise.resolve({ items: [] });
+  }
+
+  public async findById(id: number): Promise<ArticleGetItemResponseDto | null> {
+    const article = await this.articleRepository.findById(id);
+
+    if (!article) {
+      return null;
+    }
+
+    return article.toObject();
+  }
+
+  public async create(
+    payload: ArticleCreateDto,
+  ): Promise<ArticleCreateResponseDto> {
+    const article = await this.articleRepository.create(
+      ArticleEntity.initializeNew({
+        title: payload.title,
+        text: payload.text,
+        userId: payload.userId,
+        promptId: payload?.promptId ?? null,
+        genreId: payload.genreId,
+        publishedAt: payload?.publishedAt ?? null,
+      }),
+    );
+
+    return article.toObject();
+  }
+
+  public async update(
+    id: number,
+    payload: ArticleUpdateRequestDto,
+  ): Promise<ArticleUpdateResponseDto> {
+    const article = await this.findById(id);
+
+    if (!article) {
+      throw new ApplicationError({
+        message: `Article with id ${id} not found`,
+      });
+    }
+
+    const updatedArticle = await this.articleRepository.update(
+      ArticleEntity.initialize({
+        ...article,
+        ...payload,
+      }),
+    );
+
+    return updatedArticle.toObject();
+  }
+
+  public delete(): Promise<boolean> {
+    return Promise.resolve(false);
+  }
+}
+
+export { ArticleService };
