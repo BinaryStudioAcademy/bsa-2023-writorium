1ac77dd6220309c3c682f42c206591060dc57f49
Oleg Hnoievets <olegg7548@gmail.com>
2023-08-24 17:21:01 +0300

    wr-43: + Modal component

---
+++ frontend/src/libs/components/modal/modal.tsx
@@ -0,0 +1,62 @@
+import { createRef } from 'react';
+
+import { Portal } from '~/libs/components/components.js';
+import { useCallback, useEffect } from '~/libs/hooks/hooks.js';
+
+import styles from './styles.module.scss';
+
+type Properties = {
+  children: React.ReactNode;
+  onClose: () => void;
+  isOpen: boolean;
+};
+
+const Modal: React.FC<Properties> = ({
+  isOpen,
+  onClose: handleOnClose,
+  children,
+}) => {
+  const outsideModalDivReference = createRef<HTMLDivElement>();
+
+  const handleClickOutsideModal = useCallback(
+    (event: MouseEvent): void => {
+      if (event.target !== outsideModalDivReference.current) {
+        return;
+      }
+      handleOnClose();
+    },
+    [handleOnClose, outsideModalDivReference],
+  );
+
+  useEffect(() => {
+    const outsideModalDiv = outsideModalDivReference.current;
+    if (!outsideModalDiv) {
+      return;
+    }
+    outsideModalDiv.addEventListener('click', handleClickOutsideModal);
+
+    return () => {
+      outsideModalDiv.removeEventListener('click', handleClickOutsideModal);
+    };
+  }, [outsideModalDivReference, handleClickOutsideModal]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <Portal>
+      <div className={styles.modal} ref={outsideModalDivReference}>
+        <div className={styles.content} role="button" tabIndex={0}>
+          <button className={styles.closeBtn} onClick={handleOnClose}>
+            x
+          </button>
+
+          {children}
+        </div>
+      </div>
+    </Portal>
+  );
+};
+
+export { Modal };
