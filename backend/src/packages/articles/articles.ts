ed9f1e8a4ce173db381115347e1592ffb91bc344
drDramen <vvpalchyn@gmail.com>
2023-08-25 01:09:17 +0300

    wr-31: + create article instances

---
+++ backend/src/packages/articles/articles.ts
@@ -0,0 +1,13 @@
+import { logger } from '~/libs/packages/logger/logger.js';
+
+import { ArticleController } from './article.controller.js';
+import { ArticleModel } from './article.model.js';
+import { ArticleRepository } from './article.repository.js';
+import { ArticleService } from './article.service.js';
+
+const articleRepository = new ArticleRepository(ArticleModel);
+const articleService = new ArticleService(articleRepository);
+const articleController = new ArticleController(logger, articleService);
+
+export { articleController, articleService };
+export { ArticleModel } from './article.model.js';
