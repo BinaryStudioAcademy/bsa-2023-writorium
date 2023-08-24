a14f4f1bd0dd603dd16f223bbf6f4a6ef3cbf1c9
Andrew <bobrovskyandrew@gmail.com>
2023-08-24 20:33:52 +0300

    wr-6: - remove magic numbers from validation-message-enum file

--- shared/src/packages/users/libs/enums/user-validation-message.enum.ts
+++ shared/src/packages/users/libs/enums/user-validation-message.enum.ts
@@ -1,8 +1,10 @@
+import { UserValidationRule } from './user-validation-rule.enum.js';
+
 const UserValidationMessage = {
   EMAIL_REQUIRE: 'This field is required',
   EMAIL_WRONG: 'Please, enter a correct email',
   PASSWORD_REQUIRE: 'This field is required',
-  PASSWORD_LENGTH: 'Your password must be at 4-20 characters long',
+  PASSWORD_LENGTH: `Your password must be at ${UserValidationRule.PASSWORD_MIN_LENGTH}-${UserValidationRule.PASSWORD_MAX_LENGTH} characters long`,
 } as const;
 
 export { UserValidationMessage };
