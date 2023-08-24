85faf6849c14a631e018b902acaeda532de15ced
drDramen <vvpalchyn@gmail.com>
2023-08-25 00:15:56 +0300

    wr-31: * update IRepository interface

--- backend/src/libs/interfaces/repository.interface.ts
+++ backend/src/libs/interfaces/repository.interface.ts
@@ -2,7 +2,7 @@ interface IRepository<T = unknown> {
   find(): Promise<T>;
   findAll(): Promise<T[]>;
   create(payload: unknown): Promise<T>;
-  update(): Promise<T>;
+  update(payload: unknown): Promise<T>;
   delete(): Promise<boolean>;
 }
 
