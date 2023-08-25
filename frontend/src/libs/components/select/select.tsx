80be26d92a0500c57db4cd27c4d8735a592f99f0
canterbery <wotphantommail@gmail.com>
2023-08-25 09:49:16 +0300

    wr-21: -  defaultStyles

--- frontend/src/libs/components/select/select.tsx
+++ frontend/src/libs/components/select/select.tsx
@@ -4,7 +4,7 @@ import {
   type FieldPath,
   type FieldValues,
 } from 'react-hook-form';
-import { type GroupBase, type Props, type StylesConfig } from 'react-select';
+import { type GroupBase, type Props } from 'react-select';
 import { default as ReactSelect } from 'react-select';
 
 import { useCallback, useFormController } from '~/libs/hooks/hooks.js';
@@ -14,7 +14,7 @@ import {
   DropdownIndicator,
   IndicatorSeparator,
 } from './libs/components/components.js';
-import { Colors } from './libs/enums/colors.enum.js';
+import { getDefaultStyles } from './libs/constants/constants.js';
 import cssStyles from './styles.module.scss';
 
 type Properties<
@@ -45,46 +45,6 @@ const Select = <
   const error = errors[name]?.message;
   const hasError = Boolean(error);
 
-  const defaultStyles: StylesConfig<SelectOption, IsMulti, Group> = {
-    dropdownIndicator: (styles, state) => ({
-      ...styles,
-      transform: state.selectProps.menuIsOpen ? 'rotate(180deg)' : '',
-      color: Colors.DARK_GREEN,
-      cursor: 'pointer',
-    }),
-    control: (styles, state) => ({
-      ...styles,
-      padding: '5px 10px 5px 20px',
-      borderRadius: '5px',
-      border:
-        state.isFocused || state.hasValue || state.menuIsOpen
-          ? `1px solid ${Colors.LIGHT_GREEN}`
-          : '1px solid transparent',
-      ':hover': {
-        cursor: 'pointer',
-        border: `1px solid ${Colors.LIGHT_GREEN}`,
-      },
-      ':active': {
-        border: `1px solid ${Colors.LIGHT_GREEN}`,
-      },
-      boxShadow: 'none',
-    }),
-    option: (styles, state) => ({
-      ...styles,
-      backgroundColor: state.isSelected ? Colors.LIGHT_GREEN : 'inherit',
-      ':active': {
-        backgroundColor: 'none',
-      },
-      ':hover': {
-        backgroundColor: state.isSelected
-          ? Colors.LIGHT_GREEN
-          : Colors.LIGHT_GREY,
-        cursor: 'pointer',
-      },
-    }),
-    placeholder: (styles) => ({ ...styles, fontSize: '13px' }),
-  };
-
   const handleChange = useCallback(
     (option: unknown) => {
       field.onChange((option as SelectOption).value);
@@ -99,7 +59,7 @@ const Select = <
         {...restProperties}
         onChange={handleChange}
         placeholder={placeholder}
-        styles={{ ...defaultStyles, ...styles }}
+        styles={{ ...getDefaultStyles<IsMulti, Group>(), ...styles }}
         components={{ DropdownIndicator, IndicatorSeparator }}
       />
       {hasError && <span className={cssStyles.error}>{error as string}</span>}
