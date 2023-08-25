45aa53bac78dda2beaf925d2a2ac59c38fd137c3
Oleg Hnoievets <olegg7548@gmail.com>
2023-08-25 19:37:54 +0300

    wr-43: * name of toggle Modal state function

--- frontend/src/libs/hooks/use-modal/use-modal.hook.ts
+++ frontend/src/libs/hooks/use-modal/use-modal.hook.ts
@@ -2,19 +2,19 @@ import { useState } from '~/libs/hooks/hooks.js';
 
 type ReturnValue = {
   isOpen: boolean;
-  toggle: () => void;
+  handleToggleModalOpen: () => void;
 };
 
 const useModal = (): ReturnValue => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = (): void => {
+  const handleToggleModalOpen = (): void => {
     setIsOpen(!isOpen);
   };
 
   return {
     isOpen,
-    toggle,
+    handleToggleModalOpen,
   };
 };
 
