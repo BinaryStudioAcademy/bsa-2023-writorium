cc00de97baeed0d0f8acacfb9645b09e55436991
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-08-26 09:35:51 +0300

    wr-51: + tag component

--- frontend/src/libs/components/components.ts
+++ frontend/src/libs/components/components.ts
@@ -7,5 +7,6 @@ export { Link } from './link/link.js';
 export { Notification } from './notification/notification.js';
 export { ProtectedRoute } from './protected-route/protected-route.jsx';
 export { RouterProvider } from './router-provider/router-provider.jsx';
+export { Tag } from './tag/tag.js';
 export { Provider as StoreProvider } from 'react-redux';
 export { Navigate, Outlet as RouterOutlet } from 'react-router-dom';
--- frontend/src/pages/articles/components/article-card/article-card.tsx
+++ frontend/src/pages/articles/components/article-card/article-card.tsx
@@ -1,10 +1,15 @@
 import ArticlePreview from '~/assets/img/article-preview.png';
 import { Avatar } from '~/libs/components/avatar/avatar.js';
-import { Icon, Link } from '~/libs/components/components.js';
+import { Icon, Link, Tag } from '~/libs/components/components.js';
 import { AppRoute } from '~/libs/enums/app-route.enum.js';
 import { type UserSignUpResponseDto } from '~/packages/users/users.js';
 
 import styles from './styles.module.scss';
+
+type TagType = {
+  id: number;
+  name: string;
+};
 
 type ArticleType = {
   publishedAt: string;
@@ -15,6 +20,7 @@ type ArticleType = {
   views: string;
   likes: string;
   dislikes: string;
+  tags: TagType[];
 };
 
 type Properties = {
@@ -22,12 +28,20 @@ type Properties = {
   user: UserSignUpResponseDto;
 };
 
-const MOCKED_TEXT = `The developer technology landscape changes all the time as new tools
+const MOCKED_TEXT: string = `The developer technology landscape changes all the time as new tools
   and technologies are introduced. After having lots of interviews and
   reading through countless job descriptions on job boards I think this
   is a great modern tech stack for JavaScript developers in 2021.`;
 
-const MOCKED_ARTICLE = {
+const MOCKED_TAGS: TagType[] = [
+  { id: 1, name: 'IT' },
+  { id: 2, name: 'CODE' },
+  { id: 3, name: 'Humor' },
+  { id: 4, name: 'Work' },
+  { id: 5, name: 'Tech' },
+];
+
+const MOCKED_ARTICLE: ArticleType = {
   publishedAt: 'May 28',
   timeSincePublication: '7 min read',
   title: 'Modern Full-Stack Developer Tech Stack 2021',
@@ -36,9 +50,10 @@ const MOCKED_ARTICLE = {
   views: '367K',
   likes: '36K',
   dislikes: '18K',
+  tags: MOCKED_TAGS,
 };
 
-const MOCKED_USER = {
+const MOCKED_USER: UserSignUpResponseDto = {
   id: 7,
   email: 'nolanaris@gmail.com',
   firstName: 'Nolan',
@@ -58,10 +73,12 @@ const ArticleCard: React.FC<Properties> = ({
     views = '367K',
     likes = '36K',
     dislikes = '18K',
+    tags: propertyTags,
   } = article;
 
   const { firstName, lastName } = user;
   const fullName = `${firstName} ${lastName}`;
+  const tags = propertyTags.map((tag) => <Tag key={tag.id} name={tag.name} />);
 
   return (
     <article className={styles.article}>
@@ -80,13 +97,7 @@ const ArticleCard: React.FC<Properties> = ({
         <div>
           <h4 className={styles.title}>{title}</h4>
           <p className={styles.text}>{text}</p>
-          <div className={styles.tags}>
-            <span className={styles.tag}>IT</span>
-            <span className={styles.tag}>CODE</span>
-            <span className={styles.tag}>Humor</span>
-            <span className={styles.tag}>Work</span>
-            <span className={styles.tag}>Tech</span>
-          </div>
+          <div className={styles.tags}>{tags}</div>
         </div>
         <img
           src={ArticlePreview}
@@ -114,8 +125,8 @@ const ArticleCard: React.FC<Properties> = ({
             <Icon iconName="dislike" className={styles.reactionIcon} />
             <span className={styles.reactionCount}>{dislikes}</span>
           </div>
+          <Icon iconName="share" className={styles.reactionIcon} />
         </div>
-        <Icon iconName="share" className={styles.reactionIcon} />
         <Link to={AppRoute.ROOT} className={styles.readMore}>
           Read more
         </Link>
