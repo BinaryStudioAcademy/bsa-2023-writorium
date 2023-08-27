f89dbcc24db5fcf0933cfc15fb6410c22206c031
Andrew <bobrovskyandrew@gmail.com>
2023-08-27 19:34:58 +0300

    wr-52: + add icon-button component

---
+++ frontend/src/libs/components/icon-button/icon-button.tsx
@@ -0,0 +1,33 @@
+import { ButtonType } from '~/libs/enums/button-type.enum.js';
+import { getValidClassNames } from '~/libs/helpers/helpers.js';
+
+import { type IconName } from '../icon/common.js';
+import { Icon } from '../icon/icon.jsx';
+import styles from './styles.module.scss';
+
+type Properties = {
+  iconName: IconName;
+  onClick?: () => void;
+  label?: string;
+  className?: string;
+  iconClassName?: string;
+};
+
+const IconButton: React.FC<Properties> = ({
+  iconName,
+  onClick,
+  label = '',
+  className = '',
+  iconClassName = '',
+}) => (
+  <button
+    className={getValidClassNames(styles.iconButton, className)}
+    type={ButtonType.BUTTON}
+    onClick={onClick}
+  >
+    <Icon iconName={iconName} className={iconClassName} />
+    {label}
+  </button>
+);
+
+export { IconButton };
