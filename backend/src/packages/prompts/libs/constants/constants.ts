c24434248c98ca3375c3ef2c4dee822125a77748
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-08-28 22:47:40 +0300

    wr-76: + swagger docs

--- backend/src/packages/prompts/libs/constants/constants.ts
+++ backend/src/packages/prompts/libs/constants/constants.ts
@@ -1,4 +1,4 @@
 const ARTICLE_PROMPT_COMPLETION_MESSAGE =
-  'Your task is to generate a prompt to write an article which will consist of the following components: genre, character, prop, situation and setting. You should randomly generate a value for each, so all of the elements create a consise and interesting article concept. Provide your output in json format with the keys: genre, prop, character, situation, setting.';
+  'Your task is to generate a prompt for writing an article which will consist of the following components: genre, character, prop, situation and setting. You should generate a random value for each, so all of the elements create a consise and interesting article concept. Provide your output in json format with the keys: genre, prop, character, situation, setting.';
 
 export { ARTICLE_PROMPT_COMPLETION_MESSAGE };
--- backend/src/packages/prompts/prompt.controller.ts
+++ backend/src/packages/prompts/prompt.controller.ts
@@ -7,6 +7,24 @@ import { type ILogger } from '~/libs/packages/logger/logger.js';
 import { PromptsApiPath } from './libs/enums/enums.js';
 import { type PromptService } from './prompt.service.js';
 
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *      GeneratedArticlePrompt:
+ *        type: object
+ *        properties:
+ *          genre:
+ *            type: string
+ *          setting:
+ *            type: string
+ *          character:
+ *            type: string
+ *          prop:
+ *            type: string
+ *          situation:
+ *            type: string
+ */
 class PromptsController extends Controller {
   private promptService: PromptService;
 
@@ -22,6 +40,20 @@ class PromptsController extends Controller {
     });
   }
 
+  /**
+   * @swagger
+   * /prompts/generate:
+   *    get:
+   *      summary: Generate random prompt for an article
+   *      description: Generate random prompt for an article
+   *      responses:
+   *        200:
+   *          description: Successful operation
+   *          content:
+   *            application/json:
+   *              schema:
+   *                $ref: '#/components/schemas/GeneratedArticlePrompt'
+   */
   public async generate(): Promise<ApiHandlerResponse> {
     return {
       status: HttpCode.OK,
