9c595e33521cb4394b54ede606f0ba59bb1905f2
drDramen <vvpalchyn@gmail.com>
2023-08-29 19:10:51 +0300

    wr-60: + add ProfileEditForm

--- frontend/src/pages/profile/profile.tsx
+++ frontend/src/pages/profile/profile.tsx
@@ -1,7 +1,18 @@
 import { Avatar } from '~/libs/components/avatar/avatar.js';
 import { Button } from '~/libs/components/components.js';
-import { useAppSelector } from '~/libs/hooks/hooks.js';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useCallback,
+  useState,
+} from '~/libs/hooks/hooks.js';
+import {
+  type UserAuthResponseDto,
+  type UserUpdateRequestDto,
+} from '~/packages/users/users.js';
+import { actions as usersActions } from '~/slices/users/users.js';
 
+import { ProfileEditForm } from './components/profile-edit-form/profile-edit-form.js';
 import styles from './styles.module.scss';
 
 const mockedUser = {
@@ -12,12 +23,23 @@ const mockedUser = {
 };
 
 const Profile: React.FC = () => {
+  const [editMode, setEditMode] = useState(false);
   const { user } = useAppSelector(({ auth }) => ({
-    user: auth.user,
+    user: auth.user as UserAuthResponseDto,
   }));
+  const dispatch = useAppDispatch();
 
   const currentUser = user ?? mockedUser;
   const userName = `${currentUser.firstName} ${currentUser.lastName}`;
+
+  const handleUpdateUser = useCallback(
+    (payload: UserUpdateRequestDto): void => {
+      void dispatch(usersActions.updateUser(payload));
+    },
+    [dispatch],
+  );
+
+  const handleEditMode = useCallback((value = true) => setEditMode(value), []);
 
   return (
     <div className={styles.profile}>
@@ -27,11 +49,25 @@ const Profile: React.FC = () => {
           avatarUrl={null}
           className={styles.avatar}
         />
-        <div className={styles.userInfo}>
-          <p>{userName}</p>
-          <p>{currentUser.email}</p>
-        </div>
-        <Button label="Edit profile" className={styles.editProfileBtn} />
+        {editMode ? (
+          <ProfileEditForm
+            user={currentUser}
+            onUpdateUser={handleUpdateUser}
+            onEdit={handleEditMode}
+          />
+        ) : (
+          <>
+            <div className={styles.userInfo}>
+              <p>{userName}</p>
+              <p>{currentUser.email}</p>
+            </div>
+            <Button
+              label="Edit profile"
+              className={styles.editProfileBtn}
+              onClick={handleEditMode}
+            />
+          </>
+        )}
       </div>
     </div>
   );
