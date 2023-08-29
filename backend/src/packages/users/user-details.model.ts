714c9d5f4405f401e847fd8e7293726e6b759ba6
drDramen <vvpalchyn@gmail.com>
2023-08-29 23:57:22 +0300

    wr-60: * use composeDatabaseRelationPath

--- backend/src/packages/users/user-details.model.ts
+++ backend/src/packages/users/user-details.model.ts
@@ -4,6 +4,7 @@ import {
   AbstractModel,
   DatabaseTableName,
 } from '~/libs/packages/database/database.js';
+import { composeDatabaseRelationPath } from '~/libs/packages/database/libs/helpers/helpers.js';
 
 import { UserModel } from './user.model.js';
 
@@ -24,8 +25,14 @@ class UserDetailsModel extends AbstractModel {
         relation: Model.HasOneRelation,
         modelClass: UserModel,
         join: {
-          from: `${DatabaseTableName.USER_DETAILS}.userId`,
-          to: `${DatabaseTableName.USERS}.id`,
+          from: composeDatabaseRelationPath<UserDetailsModel>(
+            DatabaseTableName.USER_DETAILS,
+            'userId',
+          ),
+          to: composeDatabaseRelationPath<UserModel>(
+            DatabaseTableName.USERS,
+            'id',
+          ),
         },
       },
     };
