935adb22184c9243393710ee905afeb16688ef8e
Iryna Konovalova <iconovalova19@gmail.com>
2023-08-30 16:18:40 +0200

    wr-93: * change user validation pattern rule

--- shared/src/packages/users/libs/enums/user-validation-rule.enum.ts
+++ shared/src/packages/users/libs/enums/user-validation-rule.enum.ts
@@ -1,9 +1,13 @@
 const UserValidationRule = {
   PASSWORD_MIN_LENGTH: 4,
   PASSWORD_MAX_LENGTH: 20,
-  EMAIL_PATTERN: /^[\w%+.-]+@[\d.A-Za-z-]+\.[A-Za-z]{2,}$/,
-  PASSWORD_PATTERN: /^[\w!#$%&()*+,.:;<=>?@^{}~-]*$/,
-  NAME_PATTERN: /^[A-Za-z]+$/,
+  EMAIL_PATTERN:
+    /^(?=.{1,64}@.{1,255}$)[\w.-]+(?<!\.)(?<!\.\.)@[\dA-Za-z-]+(?:\.[\dA-Za-z-]+)*$/,
+  PASSWORD_PATTERN:
+    /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!#$%&()*+,.:;<=>?@[\]^_{}~-])[\w!#$%&()*+,.:;<=>?@^{}~-]$/,
+  NAME_PATTERN: /^[ A-Za-z-]$/,
+  NAME_MIN_LENGTH: 1,
+  NAME_MAX_LENGTH: 64,
 } as const;
 
 export { UserValidationRule };
