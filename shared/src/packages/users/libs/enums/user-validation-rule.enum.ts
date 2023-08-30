7ca10bd6a263a76fbf2a2700e4e4ad791525b96f
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-08-30 13:03:14 +0300

    wr-8: + missing symbols to password pattern

--- shared/src/packages/users/libs/enums/user-validation-rule.enum.ts
+++ shared/src/packages/users/libs/enums/user-validation-rule.enum.ts
@@ -2,7 +2,7 @@ const UserValidationRule = {
   PASSWORD_MIN_LENGTH: 4,
   PASSWORD_MAX_LENGTH: 20,
   EMAIL_PATTERN: /^[\w%+.-]+@[\d.A-Za-z-]+\.[A-Za-z]{2,}$/,
-  PASSWORD_PATTERN: /^[\w!#$%&()*+,.:;<=>?@^{}~-]*$/,
+  PASSWORD_PATTERN: /^[\w!"#$%&'()*+,./:;<=>?@[\\\]^`{|}~”-]*$/,
   NAME_PATTERN: /^[A-Za-z]+$/,
 } as const;
 
