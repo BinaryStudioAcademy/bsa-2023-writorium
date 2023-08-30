0b9b47ad16c3db6dfa7d6e91584c65a1666fbfa7
Andrew <bobrovskyandrew@gmail.com>
2023-08-30 17:35:34 +0300

    wr-73: - remove files from other PR

--- frontend/src/libs/types/article.type.ts
+++
@@ -1,9 +0,0 @@
-import { type TagType } from './tag.type.js';
-
-type ArticleType = {
-  title: string;
-  text: string;
-  tags: TagType[];
-};
-
-export { type ArticleType };
--- frontend/src/libs/types/types.ts
+++ frontend/src/libs/types/types.ts
@@ -1,7 +1,5 @@
-export { type ArticleType } from './article.type.js';
 export { type AsyncThunkConfig } from './async-thunk-config.type.js';
 export { type SelectOption } from './select-option.type.js';
-export { type TagType } from './tag.type.js';
 export {
   type ServerErrorDetail,
   type ServerErrorResponse,
