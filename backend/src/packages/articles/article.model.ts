c4de4d3ce0a9141f4bd564c4fbde2a852d67797f
Oleg Hnoievets <olegg7548@gmail.com>
2023-08-30 22:48:48 +0300

    wr-77: + relationMappings to article model

--- backend/src/packages/articles/article.model.ts
+++ backend/src/packages/articles/article.model.ts
@@ -1,7 +1,13 @@
+import { Model, type RelationMappings } from 'objection';
+
 import {
   AbstractModel,
   DatabaseTableName,
 } from '~/libs/packages/database/database.js';
+import { composeDatabaseRelationPath } from '~/libs/packages/database/libs/helpers/helpers.js';
+
+import { ArticleReactionModel } from '../article-reactions/article-reaction.model.js';
+import { UserModel } from '../users/user.model.js';
 
 class ArticleModel extends AbstractModel {
   public 'title': string;
@@ -13,6 +19,30 @@ class ArticleModel extends AbstractModel {
 
   public static override get tableName(): string {
     return DatabaseTableName.ARTICLES;
+  }
+
+  public static get relationMappings(): RelationMappings {
+    return {
+      articleRactions: {
+        relation: Model.HasManyRelation,
+        modelClass: ArticleReactionModel,
+        join: {
+          from: composeDatabaseRelationPath('articles', 'id'),
+          to: composeDatabaseRelationPath<ArticleReactionModel>(
+            'article_reactions',
+            'articleId',
+          ),
+        },
+      },
+      user: {
+        relation: Model.HasOneRelation,
+        modelClass: UserModel,
+        join: {
+          from: composeDatabaseRelationPath<ArticleModel>('articles', 'userId'),
+          to: composeDatabaseRelationPath('users', 'id'),
+        },
+      },
+    };
   }
 }
 
