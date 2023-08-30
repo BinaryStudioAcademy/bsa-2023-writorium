6ff43fc1d129a6c9bfddd50e73465e6da7899b32
Oleg Hnoievets <olegg7548@gmail.com>
2023-08-30 22:37:28 +0300

    wr-77: + article_reactions to DatabaseTableName

--- backend/src/libs/packages/database/libs/enums/database-table-name.enum.ts
+++ backend/src/libs/packages/database/libs/enums/database-table-name.enum.ts
@@ -1,6 +1,7 @@
 const DatabaseTableName = {
   MIGRATIONS: 'migrations',
   ARTICLES: 'articles',
+  ARTICLE_REACTIONS: 'article_reactions',
   USERS: 'users',
   USER_DETAILS: 'user_details',
 } as const;
