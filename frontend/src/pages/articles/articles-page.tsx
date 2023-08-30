28f956a0b4dcb06d18aa058bf851b2d3ffc3a03a
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-08-30 16:07:06 +0300

    wr-75: * implement active tabs with navlink state

--- frontend/src/pages/articles/articles-page.tsx
+++ frontend/src/pages/articles/articles-page.tsx
@@ -1,32 +1,23 @@
 import { Link, RouterOutlet } from '~/libs/components/components.js';
 import { ArticleSubRoute } from '~/libs/enums/enums.js';
-import { getValidClassNames } from '~/libs/helpers/helpers.js';
-import { useLocation } from '~/libs/hooks/hooks.js';
 
 import styles from './styles.module.scss';
 
 const ArticlesPage: React.FC = () => {
-  const { pathname } = useLocation();
-  const activePage = pathname.split('/').pop();
-
   return (
     <div className={styles.wrapper}>
       <div className={styles.tabsWrapper}>
         <Link
           to={ArticleSubRoute.FEED}
-          className={getValidClassNames(
-            styles.tab,
-            activePage === ArticleSubRoute.FEED && styles.activeTab,
-          )}
+          className={styles.tab}
+          activeClassName={styles.activeTab}
         >
           Feed
         </Link>
         <Link
           to={ArticleSubRoute.MY_ARTICLES}
-          className={getValidClassNames(
-            styles.tab,
-            activePage === ArticleSubRoute.MY_ARTICLES && styles.activeTab,
-          )}
+          className={styles.tab}
+          activeClassName={styles.activeTab}
         >
           My articles
         </Link>
