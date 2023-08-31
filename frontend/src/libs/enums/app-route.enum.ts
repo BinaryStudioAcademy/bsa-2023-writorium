9456c910ee6ab657efa7940471ad75f76c43ba9a
canterbery <wotphantommail@gmail.com>
2023-08-31 15:51:15 +0300

    wr-80: + change password routes

--- frontend/src/libs/enums/app-route.enum.ts
+++ frontend/src/libs/enums/app-route.enum.ts
@@ -1,9 +1,13 @@
+import { RESET_PASSWORD_ROUTE } from '../constants/constants.js';
+
 const AppRoute = {
   ROOT: '/',
   SIGN_IN: '/sign-in',
   SIGN_UP: '/sign-up',
   PROFILE: '/profile',
   ARTICLE: '/articles/:id',
+  FORGOT_PASSWORD: '/forgot-password',
+  RESET_PASSWORD: RESET_PASSWORD_ROUTE.WITH_PARAM,
 } as const;
 
 export { AppRoute };
