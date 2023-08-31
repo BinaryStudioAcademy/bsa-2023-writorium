a46e29809b8ab17724b1f626b850c82b35ea534e
canterbery <wotphantommail@gmail.com>
2023-08-31 12:00:13 +0300

    wr-80: + dto types exports

--- shared/src/packages/auth/auth.ts
+++ shared/src/packages/auth/auth.ts
@@ -1,1 +1,7 @@
 export { AuthApiPath } from './libs/enums/enums.js';
+export {
+  type AuthRequestPasswordDto,
+  type AuthResetPasswordDto,
+} from './libs/types/types.js';
+export { requestPassword as requestPasswordValidationSchema } from './libs/validation-schemas/request-password.validation-schema.js';
+export { resetPassword as resetPasswordValidationSchema } from './libs/validation-schemas/reset-password.validation-schema.js';
