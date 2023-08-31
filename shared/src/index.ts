b0915169e2d07e5034aa53ea9fb45f9918de4dee
Oleg Hnoievets <olegg7548@gmail.com>
2023-08-31 17:27:37 +0300

    wr-77: * prettify

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -26,6 +26,12 @@ export {
   type WithNullableKeys,
 } from './libs/types/types.js';
 export {
+  type Achievement,
+  type AchievementBaseResponseDto,
+  type AchievementGetAllResponseDto,
+  AchievementsApiPath,
+} from './packages/achievements/achievements.js';
+export {
   type ArticleReactionCreateDto,
   type ArticleReactionCreateResponseDto,
   type ArticleReactionEntityType,
@@ -33,12 +39,6 @@ export {
   type ArticleReactionResponseDto,
   articleReactionValidationSchema,
 } from './packages/article-reactions/article-reactions.js';
-export{
-  type Achievement,
-  type AchievementBaseResponseDto,
-  type AchievementGetAllResponseDto,
-  AchievementsApiPath,
-} from './packages/achievements/achievements.js';
 export {
   type ArticleBaseResponseDto,
   type ArticleCreateDto,
