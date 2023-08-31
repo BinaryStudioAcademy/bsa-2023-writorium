6670c84166173d291edafcd93eeb90dd184f5dc2
canterbery <wotphantommail@gmail.com>
2023-08-31 13:34:41 +0300

    wr-80: + schemas

--- shared/src/packages/auth/auth.ts
+++ shared/src/packages/auth/auth.ts
@@ -3,5 +3,7 @@ export {
   type AuthRequestPasswordDto,
   type AuthResetPasswordDto,
 } from './libs/types/types.js';
-export { requestPassword as requestPasswordValidationSchema } from './libs/validation-schemas/request-password.validation-schema.js';
-export { resetPassword as resetPasswordValidationSchema } from './libs/validation-schemas/reset-password.validation-schema.js';
+export {
+  requestPassword as requestPasswordValidationSchema,
+  resetPassword as resetPasswordValidationSchema,
+} from './libs/validation-schemas/validation-schemas.js';
