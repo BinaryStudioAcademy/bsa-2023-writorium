8cbbcc62f73429887d072e0174e831e2d3615bc6
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-08-31 19:08:17 +0300

    wr-8: - rollback password pattern change

--- shared/src/packages/users/libs/enums/user-validation-rule.enum.ts
+++ shared/src/packages/users/libs/enums/user-validation-rule.enum.ts
@@ -1,9 +1,9 @@
 const UserValidationRule = {
   PASSWORD_MIN_LENGTH: 4,
   PASSWORD_MAX_LENGTH: 20,
+  PASSWORD_PATTERN: /^[\w!#$%&()*+,.:;<=>?@^{}~-]*$/,
   EMAIL_PATTERN:
     /^[\d!#$%&'*+/=?^_`a-z{|}~-]+(?:\.[\d!#$%&'*+/=?^_`a-z{|}~-]+)*@(?:[\da-z](?:[\da-z-]*[\da-z])?\.)+[\da-z](?:[\da-z-]*[\da-z])?$/,
-  PASSWORD_PATTERN: /^[\w!"#$%&'()*+,./:;<=>?@[\\\]^`{|}~”-]*$/,
   NAME_PATTERN: /^[A-Za-z]+$/,
 } as const;
 
