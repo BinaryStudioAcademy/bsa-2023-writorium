3f48e57061b8206da4384c2f424d9aa3acee20bc
drDramen <vvpalchyn@gmail.com>
2023-08-31 18:56:54 +0300

    wr-95: + add relation avatarId to files id

--- backend/src/packages/files/file.model.ts
+++ backend/src/packages/files/file.model.ts
@@ -1,13 +1,36 @@
+import { Model, type RelationMappings } from 'objection';
+
 import {
   AbstractModel,
   DatabaseTableName,
 } from '~/libs/packages/database/database.js';
+import { composeDatabaseRelationPath } from '~/libs/packages/database/libs/helpers/compose-database-relation-path/compose-database-relation-path.helper.js';
+import { UserDetailsModel } from '~/packages/users/user-details.model.js';
 
 class FileModel extends AbstractModel {
   public 'url': string;
 
   public static override get tableName(): string {
     return DatabaseTableName.FILES;
+  }
+
+  public static get relationMappings(): RelationMappings {
+    return {
+      userDetails: {
+        relation: Model.HasOneRelation,
+        modelClass: UserDetailsModel,
+        join: {
+          from: composeDatabaseRelationPath<FileModel>(
+            DatabaseTableName.FILES,
+            'id',
+          ),
+          to: composeDatabaseRelationPath<UserDetailsModel>(
+            DatabaseTableName.USER_DETAILS,
+            'avatarId',
+          ),
+        },
+      },
+    };
   }
 }
 
