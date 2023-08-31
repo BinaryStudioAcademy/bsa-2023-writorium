984038aed97fa9da991a9a1677d6c0baa7ad8b4f
canterbery <wotphantommail@gmail.com>
2023-08-31 13:09:01 +0300

    wr-80: + email failed to send exception

--- backend/src/libs/packages/exceptions/exceptions.ts
+++ backend/src/libs/packages/exceptions/exceptions.ts
@@ -1,3 +1,4 @@
+export { EmailFailedToSendError } from './email-failed-to-send/email-failed-to-send.exception.js';
 export { FailedToGeneratePromptError } from './failed-to-generate-prompt-error/failed-to-generate-prompt-error.exception.js';
 export { FileNotProvidedError } from './file-not-provided-error/file-not-provided-error.exception.js';
 export { FileSizeLimitExceededError } from './file-size-limit-exceeded-error/file-size-limit-exceeded-error.exception.js';
