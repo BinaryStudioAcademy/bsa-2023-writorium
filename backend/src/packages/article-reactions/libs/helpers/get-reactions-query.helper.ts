1bfc9423f9b97951c74d9802ea21110f33eeda21
Oleg Hnoievets <olegg7548@gmail.com>
2023-08-31 17:06:31 +0300

    wr-77: * getReactionsQuery: improvements and using ArticleReactionCreateResponseDto

--- backend/src/packages/article-reactions/libs/helpers/get-reactions-query.helper.ts
+++ backend/src/packages/article-reactions/libs/helpers/get-reactions-query.helper.ts
@@ -1,13 +1,16 @@
 import { type QueryBuilder } from 'objection';
 
 import { type ArticleReactionModel } from '../../article-reaction.model.js';
+import { type ArticleReactionCreateResponseDto } from '../types/types.js';
 
-const getReactionsQuery =
-  (model: typeof ArticleReactionModel) =>
-  (isLike: boolean): QueryBuilder<ArticleReactionModel> => {
-    const col = isLike ? 'likeCount' : 'dislikeCount';
+const getReactionsQuery = (model: typeof ArticleReactionModel) => {
+  return (isLike: boolean): QueryBuilder<ArticleReactionModel> => {
+    const col: keyof ArticleReactionCreateResponseDto = isLike
+      ? 'likeCount'
+      : 'dislikeCount';
 
     return model.query().count().where({ isLike }).as(col);
   };
+};
 
 export { getReactionsQuery };
