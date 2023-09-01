5aa2a329101946a4e7b86c1c6d614811f5f430fc
canterbery <wotphantommail@gmail.com>
2023-09-01 14:56:29 +0300

    wr-80: * types

--- backend/src/libs/packages/token/token.package.ts
+++ backend/src/libs/packages/token/token.package.ts
@@ -1,12 +1,15 @@
 import { type JWTPayload, jwtVerify, SignJWT } from 'jose';
 
+import { type TokenExpirationTime } from '~/libs/enums/enums.js';
+import { type ValueOf } from '~/libs/types/types.js';
+
 import { type IConfig } from '../config/config.js';
 import { type IToken } from './libs/interfaces/interfaces.js';
 
 class Token implements IToken {
   private secret: Uint8Array;
   private algorithm: string;
-  private expirationTime: string;
+  private expirationTime: ValueOf<typeof TokenExpirationTime>;
 
   public constructor(config: IConfig) {
     this.secret = new TextEncoder().encode(config.ENV.JWT.SECRET_KEY);
@@ -16,7 +19,7 @@ class Token implements IToken {
 
   public create<T extends Record<string, unknown>>(
     payload: T,
-    expirationTime: string = this.expirationTime,
+    expirationTime: ValueOf<typeof TokenExpirationTime> = this.expirationTime,
   ): Promise<string> {
     return new SignJWT(payload)
       .setProtectedHeader({ alg: this.algorithm })
