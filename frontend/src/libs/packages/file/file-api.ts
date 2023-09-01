80c8dbcb6cc81e10636bf7d752d47551af99dff1
drDramen <vvpalchyn@gmail.com>
2023-09-01 19:51:56 +0300

    wr-95: * rename folder

--- frontend/src/libs/packages/file/file-api.ts
+++ frontend/src/libs/packages/file/file-api.ts
@@ -1,6 +1,6 @@
 import { config } from '~/libs/packages/config/config.js';
+import { FileApi } from '~/libs/packages/file/file-api.package.js';
 import { http } from '~/libs/packages/http/http.js';
-import { FileApi } from '~/libs/packages/image/file-api.package.js';
 import { storage } from '~/libs/packages/storage/storage.js';
 
 const fileApi = new FileApi({
