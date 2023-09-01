93ac39a704858ebb8bd2442a46f8fdd0d5794939
canterbery <wotphantommail@gmail.com>
2023-09-01 11:46:22 +0300

    wr-80: + enum

--- backend/src/libs/packages/token/token.ts
+++ backend/src/libs/packages/token/token.ts
@@ -3,5 +3,6 @@ import { Token } from './token.package.js';
 
 const token = new Token(config);
 
+export { TokenExpirationTime } from './libs/enums/enums.js';
 export { type IToken } from './libs/interfaces/interfaces.js';
 export { token };
