2338f3ebfad8981fb884d0f4144c7dbd0e4c2da8
canterbery <wotphantommail@gmail.com>
2023-09-01 19:42:38 +0300

    wr-73: + author relation

--- backend/src/packages/comments/comment.model.ts
+++ backend/src/packages/comments/comment.model.ts
@@ -1,7 +1,12 @@
+import { Model, type RelationMappings } from 'objection';
+
 import {
   AbstractModel,
   DatabaseTableName,
 } from '~/libs/packages/database/database.js';
+import { composeDatabaseRelationPath } from '~/libs/packages/database/libs/helpers/helpers.js';
+
+import { UserModel } from '../users/users.js';
 
 class CommentModel extends AbstractModel {
   public 'text': string;
@@ -10,6 +15,24 @@ class CommentModel extends AbstractModel {
 
   public static override get tableName(): string {
     return DatabaseTableName.COMMENTS;
+  }
+  public static get relationMappings(): RelationMappings {
+    return {
+      author: {
+        relation: Model.HasOneRelation,
+        modelClass: UserModel,
+        join: {
+          from: composeDatabaseRelationPath<CommentModel>(
+            DatabaseTableName.COMMENTS,
+            'userId',
+          ),
+          to: composeDatabaseRelationPath<UserModel>(
+            DatabaseTableName.USERS,
+            'id',
+          ),
+        },
+      },
+    };
   }
 }
 
