490f4e0b3ad9489fdfa77e0e5983e225b6c6bcc4
canterbery <wotphantommail@gmail.com>
2023-09-02 12:59:55 +0300

    wr-73: * validation

--- shared/src/packages/comments/libs/validation-schemas/comment-update.validation-schema.ts
+++ shared/src/packages/comments/libs/validation-schemas/comment-update.validation-schema.ts
@@ -2,8 +2,12 @@ import joi from 'joi';
 
 import { type CommentUpdateRequestDto } from '~/packages/comments/libs/types/types.js';
 
+import { CommentValidationMessage } from '../enums/enums.js';
+
 const commentUpdate = joi.object<CommentUpdateRequestDto, true>({
-  text: joi.string().trim(),
+  text: joi.string().trim().required().messages({
+    'string.empty': CommentValidationMessage.COMMENT_TEXT_REQUIRE,
+  }),
 });
 
 export { commentUpdate };
