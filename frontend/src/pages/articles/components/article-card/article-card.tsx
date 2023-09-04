109c6b8603f0d8e5ef3f70b878624c49d1c204c2
Oleg Hnoievets <olegg7548@gmail.com>
2023-09-04 18:52:08 +0300

    wr-132: * using formatDate in article-card

--- frontend/src/pages/articles/components/article-card/article-card.tsx
+++ frontend/src/pages/articles/components/article-card/article-card.tsx
@@ -1,7 +1,7 @@
 import ArticlePreview from '~/assets/img/article-preview.png';
 import { Avatar, Icon, Link } from '~/libs/components/components.js';
-import { AppRoute } from '~/libs/enums/enums.js';
-import { formatShortDate, getFullName } from '~/libs/helpers/helpers.js';
+import { AppRoute, DatePattern } from '~/libs/enums/enums.js';
+import { formatDate, getFullName } from '~/libs/helpers/helpers.js';
 import { type ArticleWithAuthorType } from '~/packages/articles/articles.js';
 import { type UserDetailsResponseDto } from '~/packages/users/users.js';
 
@@ -38,9 +38,14 @@ const ArticleCard: React.FC<Properties> = ({
           <span className={styles.publisherName}>
             {getFullName(author.firstName, author.lastName)}
           </span>
-          <span className={styles.publicationTime}>
-            {formatShortDate(publishedAt as string)}
-          </span>
+          {publishedAt && (
+            <span className={styles.publicationTime}>
+              {formatDate({
+                isoString: publishedAt,
+                pattern: `${DatePattern.MONTH_DAY} ${DatePattern.SHORT_MONTH_NAME}`,
+              })}
+            </span>
+          )}
           <span className={styles.publicationTime}>{MOCKED_READ_TIME}</span>
         </div>
         <Icon iconName="favorite" className={styles.icon} />
