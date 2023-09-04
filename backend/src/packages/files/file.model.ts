a7d9df5f326a071b27cb076c3d01f25cd7e26317
drDramen <vvpalchyn@gmail.com>
2023-09-04 09:07:42 +0300

    wr-95: - remove unnecessary relation

--- backend/src/packages/files/file.model.ts
+++ backend/src/packages/files/file.model.ts
@@ -1,36 +1,13 @@
-import { Model, type RelationMappings } from 'objection';
-
 import {
   AbstractModel,
   DatabaseTableName,
 } from '~/libs/packages/database/database.js';
-import { composeDatabaseRelationPath } from '~/libs/packages/database/libs/helpers/helpers.js';
-import { UserDetailsModel } from '~/packages/users/user-details.model.js';
 
 class FileModel extends AbstractModel {
   public 'url': string;
 
   public static override get tableName(): string {
     return DatabaseTableName.FILES;
-  }
-
-  public static get relationMappings(): RelationMappings {
-    return {
-      userDetails: {
-        relation: Model.HasOneRelation,
-        modelClass: UserDetailsModel,
-        join: {
-          from: composeDatabaseRelationPath<FileModel>(
-            DatabaseTableName.FILES,
-            'id',
-          ),
-          to: composeDatabaseRelationPath<UserDetailsModel>(
-            DatabaseTableName.USER_DETAILS,
-            'avatarId',
-          ),
-        },
-      },
-    };
   }
 }
 
