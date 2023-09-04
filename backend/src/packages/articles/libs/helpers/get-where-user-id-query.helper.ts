1a1e3e738fd10435021362b14274226df0b07096
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-09-04 08:37:40 +0300

    wr-75: + arrow brackets to the function

--- backend/src/packages/articles/libs/helpers/get-where-user-id-query.helper.ts
+++ backend/src/packages/articles/libs/helpers/get-where-user-id-query.helper.ts
@@ -2,12 +2,12 @@ import { type QueryBuilder } from 'objection';
 
 import { type ArticleModel } from '../../article.model.js';
 
-const getWhereUserIdQuery =
-  (userId?: number) =>
-  (builder: QueryBuilder<ArticleModel, ArticleModel[]>): void => {
+const getWhereUserIdQuery = (userId?: number) => {
+  return (builder: QueryBuilder<ArticleModel, ArticleModel[]>): void => {
     if (userId) {
       void builder.where('articles.userId', userId);
     }
   };
+};
 
 export { getWhereUserIdQuery };
