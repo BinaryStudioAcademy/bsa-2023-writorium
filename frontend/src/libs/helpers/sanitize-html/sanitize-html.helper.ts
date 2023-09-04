a34463f88b2ef54f10c51fb0fe4273e291a63581
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-04 09:37:58 +0300

    wr-103: * use function block brackets

--- frontend/src/libs/helpers/sanitize-html/sanitize-html.helper.ts
+++ frontend/src/libs/helpers/sanitize-html/sanitize-html.helper.ts
@@ -1,11 +1,12 @@
 import sanitizeHtmlInternal from 'sanitize-html';
 
-const sanitizeHtml = (html: string): string =>
-  sanitizeHtmlInternal(html, {
+const sanitizeHtml = (html: string): string => {
+  return sanitizeHtmlInternal(html, {
     allowedAttributes: {
       ...sanitizeHtmlInternal.defaults.allowedAttributes,
       '*': ['style'],
     },
   });
+};
 
 export { sanitizeHtml };
