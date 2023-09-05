88f9daf8720d87a99fe63ed93113ef1cc4aaa401
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2023-09-05 11:30:15 +0300

    wr-111: - textarea styles; updated snake case helper

--- backend/src/libs/helpers/convert-to-snake-case/convert-to-snake-case.helper.ts
+++ backend/src/libs/helpers/convert-to-snake-case/convert-to-snake-case.helper.ts
@@ -1,5 +1,7 @@
 import snakeCase from 'lodash.snakecase';
 
-const convertToSnakeCase = (input: string): string => snakeCase(input);
+const convertToSnakeCase = (input: string): string => {
+  return snakeCase(input);
+};
 
 export { convertToSnakeCase };
