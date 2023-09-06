0b854f55f764019e01eade00692005c73a7e560c
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-09-06 18:28:56 +0300

    wr-137: + article service handler

--- backend/src/packages/articles/article.service.ts
+++ backend/src/packages/articles/article.service.ts
@@ -1,5 +1,5 @@
 import { ApplicationError } from '~/libs/exceptions/exceptions.js';
-import { safeJSONParse } from '~/libs/helpers/helpers.js';
+import { differenceInDays, safeJSONParse } from '~/libs/helpers/helpers.js';
 import { type IService } from '~/libs/interfaces/service.interface.js';
 import { type OpenAIService } from '~/libs/packages/openai/openai.package.js';
 
@@ -7,13 +7,19 @@ import { GenreEntity } from '../genres/genre.entity.js';
 import { type GenreRepository } from '../genres/genre.repository.js';
 import { ArticleEntity } from './article.entity.js';
 import { type ArticleRepository } from './article.repository.js';
-import { getDetectArticleGenreCompletionConfig } from './libs/helpers/helpers.js';
+import { DateFormat } from './libs/enums/enums.js';
+import {
+  getDetectArticleGenreCompletionConfig,
+  getFormattedDate,
+  subMonths,
+} from './libs/helpers/helpers.js';
 import {
   type ArticleBaseResponseDto,
   type ArticleCreateDto,
   type ArticleGetAllResponseDto,
   type ArticleUpdateRequestDto,
   type DetectedArticleGenre,
+  type UserActivityResponseDto,
 } from './libs/types/types.js';
 
 class ArticleService implements IService {
@@ -103,6 +109,49 @@ class ArticleService implements IService {
     }
 
     return article.toObject();
+  }
+
+  public async getUserActivity(
+    userId: number,
+  ): Promise<UserActivityResponseDto> {
+    const currentDate = new Date();
+    const sixMonthAgo = subMonths(currentDate, 6);
+    const daysInHalfYear = differenceInDays(currentDate, sixMonthAgo);
+    const halfYearActivity: UserActivityResponseDto = [];
+
+    const userActivity = await this.articleRepository.getUserActivity({
+      userId,
+      activityFrom: sixMonthAgo.toISOString(),
+      activityTo: currentDate.toISOString(),
+    });
+
+    for (let index = 0; index <= daysInHalfYear; index++) {
+      const dateForStatistic = new Date(
+        sixMonthAgo.getFullYear(),
+        sixMonthAgo.getMonth(),
+        sixMonthAgo.getDate() + index,
+      ).toISOString();
+
+      const activeDayIndex = userActivity.findIndex((activity) => {
+        return (
+          getFormattedDate(activity.date, DateFormat.YEAR_MONTH_DATE) ===
+          getFormattedDate(dateForStatistic, DateFormat.YEAR_MONTH_DATE)
+        );
+      });
+
+      if (activeDayIndex >= 0) {
+        const dayActivity = userActivity[activeDayIndex];
+        halfYearActivity.push({
+          date: dayActivity.date,
+          count: Number(dayActivity.count),
+        });
+        continue;
+      }
+
+      halfYearActivity.push({ date: dateForStatistic, count: 0 });
+    }
+
+    return halfYearActivity;
   }
 
   public async create(
