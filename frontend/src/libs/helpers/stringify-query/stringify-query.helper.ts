70f2775af76e63b453e54d977a2bdb771060593a
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-07 00:01:15 +0300

    wr-138: - queryString

--- frontend/src/libs/helpers/stringify-query/stringify-query.helper.ts
+++
@@ -1,7 +0,0 @@
-import queryString from 'query-string';
-
-const stringifyQuery = (query: Record<string, unknown>): string => {
-  return queryString.stringify(query);
-};
-
-export { stringifyQuery };
