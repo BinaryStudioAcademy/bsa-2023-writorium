0d498d1dbce46112cc5109fed9685d3e3e24a52c
canterbery <wotphantommail@gmail.com>
2023-09-06 18:26:44 +0300

    wr-153: + index

---
+++ backend/src/db/migrations/20230906150722_make_users_email_index.ts
@@ -0,0 +1,13 @@
+import { type Knex } from 'knex';
+
+async function up(knex: Knex): Promise<void> {
+  return await knex.raw(
+    'CREATE UNIQUE INDEX users_lowercase_email ON users (lower(email));',
+  );
+}
+
+async function down(knex: Knex): Promise<void> {
+  return await knex.raw('DROP index IF EXISTS users_lowercase_email');
+}
+
+export { down, up };
