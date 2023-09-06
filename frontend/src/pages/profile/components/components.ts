78e236e333f981207ca150ff16fade0722033d95
drDramen <vvpalchyn@gmail.com>
2023-09-06 20:16:56 +0300

    wr-136: * fix path

--- frontend/src/pages/profile/components/components.ts
+++ frontend/src/pages/profile/components/components.ts
@@ -1,6 +1,6 @@
 export { Achievement } from './achievement/achievement.js';
 export { AchievementList } from './achievement-list/achievement-list.js';
+export { AchievementTooltipContent } from './achievement-tooltip-content/achievement-tooltip-content.js';
 export { ProfileEditForm } from './profile-edit-form/profile-edit-form.js';
 export { UserAchievements } from './user-achievements/user-achievements.js';
 export { UserInfo } from './user-info/user-info.js';
-export { AchievementTooltipContent } from '~/pages/profile/components/achievement-tooltip-content/achievement-tooltip-content.js';
