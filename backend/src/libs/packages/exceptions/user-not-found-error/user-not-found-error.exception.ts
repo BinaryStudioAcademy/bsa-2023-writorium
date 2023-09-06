d7006b20588dca65f67ebe6255e690325cef266a
Oleg Hnoievets <olegg7548@gmail.com>
2023-09-06 16:40:04 +0300

    wr-140: - useless custom BE exceptions

--- backend/src/libs/packages/exceptions/user-not-found-error/user-not-found-error.exception.ts
+++
@@ -1,9 +0,0 @@
-import { HttpCode, HttpError } from '~/libs/packages/http/http.js';
-
-class UserNotFoundError extends HttpError {
-  public constructor() {
-    super({ message: 'User Not Found', status: HttpCode.NOT_FOUND });
-  }
-}
-
-export { UserNotFoundError };
