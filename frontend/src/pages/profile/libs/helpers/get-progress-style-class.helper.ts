245d853e0997b9527aa4015c1f21a11f3eb19d9c
drDramen <vvpalchyn@gmail.com>
2023-09-07 10:38:22 +0300

    wr-136: * use constants

--- frontend/src/pages/profile/libs/helpers/get-progress-style-class.helper.ts
+++ frontend/src/pages/profile/libs/helpers/get-progress-style-class.helper.ts
@@ -1,15 +1,16 @@
-import { type ValueOf } from 'shared/build/index.js';
+import { type ValueOf } from '~/libs/types/types.js';
 
+import { PROGRESS_MAX, PROGRESS_MIN } from '../constants/constants.js';
 import { ProgressStatusClass } from '../enums/enums.js';
 
 const getProgressStyleClass = (
   progress: number,
 ): ValueOf<typeof ProgressStatusClass> => {
-  if (progress <= 0) {
+  if (progress <= PROGRESS_MIN) {
     return ProgressStatusClass.NOT_STARTED;
   }
 
-  if (progress >= 100) {
+  if (progress >= PROGRESS_MAX) {
     return ProgressStatusClass.DONE;
   }
 
