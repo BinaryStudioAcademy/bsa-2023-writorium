b537d0036abe7d096feb4543ad32fc651d8a0a7d
canterbery <wotphantommail@gmail.com>
2023-09-07 10:14:18 +0300

    wr-153: * migration

--- backend/src/db/migrations/20230906150722_make_users_email_index.ts
+++ backend/src/db/migrations/20230906150722_make_users_email_index.ts
@@ -1,14 +1,16 @@
 import { type Knex } from 'knex';
 
 async function up(knex: Knex): Promise<void> {
-  return await knex.raw(
-    'CREATE UNIQUE INDEX IF NOT EXISTS users_lowercase_email ON users (lower(email)); alter table users drop constraint users_email_unique;',
+  await knex.raw(
+    'CREATE UNIQUE INDEX IF NOT EXISTS users_lowercase_email ON users (lower(email));',
   );
+  await knex.raw('alter table users drop constraint users_email_unique;');
 }
 
 async function down(knex: Knex): Promise<void> {
-  return await knex.raw(
-    'DROP index IF EXISTS users_lowercase_email; ALTER TABLE users ADD CONSTRAINT user_email_unique UNIQUE (email);',
+  await knex.raw('DROP index IF EXISTS users_lowercase_email;');
+  await knex.raw(
+    'ALTER TABLE users ADD CONSTRAINT user_email_unique UNIQUE (email);',
   );
 }
 
