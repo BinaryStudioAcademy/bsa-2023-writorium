449f8f3aae4750f39dade82aaff1c3ba5d2436d6
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-09-07 13:22:01 +0300

    wr-137: + arrow brackets to function

--- frontend/src/pages/profile/libs/helpers/get-unique-months/get-unique-months.helper.ts
+++ frontend/src/pages/profile/libs/helpers/get-unique-months/get-unique-months.helper.ts
@@ -3,9 +3,9 @@ import { getFormattedDate } from '~/libs/helpers/helpers.js';
 import { type UserActivityResponseDto } from '~/packages/users/users.js';
 
 const getUniqueMonths = (data: UserActivityResponseDto[]): string[] => {
-  const months = data.map((activity) =>
-    getFormattedDate(activity.date, DateFormat.SHORT_MONTH),
-  );
+  const months = data.map((activity) => {
+    return getFormattedDate(activity.date, DateFormat.SHORT_MONTH);
+  });
 
   return [...new Set(months)];
 };
