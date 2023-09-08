84afbc829a66d82de3bd30ab7eade8464811dbe4
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-08 21:45:49 +0300

    wr-167: + drag over styles to upload component

--- frontend/src/pages/article/components/article-form/libs/components/article-cover-upload/article-cover-upload.tsx
+++ frontend/src/pages/article/components/article-form/libs/components/article-cover-upload/article-cover-upload.tsx
@@ -6,7 +6,13 @@ import {
   type FieldValues,
 } from 'react-hook-form';
 
-import { Button, ErrorMessage, Icon } from '~/libs/components/components.js';
+import {
+  ErrorMessage,
+  Icon,
+  IconButton,
+} from '~/libs/components/components.js';
+import { DotLoader } from '~/libs/components/prompt-generation/libs/components/components.js';
+import { getValidClassNames } from '~/libs/helpers/helpers.js';
 import {
   useAppDispatch,
   useFormController,
@@ -22,16 +28,22 @@ type Properties<T extends FieldValues> = {
   control: Control<T, null>;
   errors: FieldErrors<T>;
   name: FieldPath<T>;
+  initialPreviewUrl?: string;
 };
 
 const ArticleCoverUpload = <T extends FieldValues>({
-  control,
-  errors,
   name,
+  errors,
+  control,
+  initialPreviewUrl,
 }: Properties<T>): React.ReactNode => {
   const dispatch = useAppDispatch();
   const { field } = useFormController({ name, control });
-  const [previewUrl, setPreviewUrl] = useState<string | null>();
+  const [previewUrl, setPreviewUrl] = useState<string | null>(
+    initialPreviewUrl ?? null,
+  );
+  const [isDragActive, setIsDragActive] = useState(false);
+  const [isUploadFileLoading, setIsUploadFileLoading] = useState(false);
 
   const error = errors[name]?.message;
   const hasError = Boolean(error);
@@ -45,6 +57,7 @@ const ArticleCoverUpload = <T extends FieldValues>({
       const formData = new FormData();
       formData.append('file', image);
 
+      setIsUploadFileLoading(true);
       void dispatch(filesActions.uploadFile(formData))
         .unwrap()
         .then((fileData) => {
@@ -54,6 +67,9 @@ const ArticleCoverUpload = <T extends FieldValues>({
         .catch((error: Error) => {
           appActions.notify({ type: 'error', message: error.message });
           field.onChange(null);
+        })
+        .finally(() => {
+          setIsUploadFileLoading(false);
         });
     }
   };
@@ -62,29 +78,50 @@ const ArticleCoverUpload = <T extends FieldValues>({
     setPreviewUrl(null);
   };
 
+  const handleDragEnter = (): void => {
+    setIsDragActive(true);
+  };
+
+  const handleDragLeave = (): void => {
+    setIsDragActive(false);
+  };
+
+  const handleDrop = (): void => {
+    setIsDragActive(false);
+  };
+
   return (
     <>
       <div className={styles.uploadWrapper}>
         <div className={styles.header}>
           <p className={styles.uploadTitle}>Add a cover image</p>
           {previewUrl && (
-            <Button
+            <IconButton
+              iconName="crossMark"
               label="Remove"
               onClick={handleRemoveCover}
               className={styles.removeButton}
             />
           )}
         </div>
+        {isUploadFileLoading && <DotLoader isLoading />}
         {previewUrl && (
           <div className={styles.coverContainer}>
             <img alt="article cover" src={previewUrl} />
           </div>
         )}
-        {!previewUrl && (
-          <div className={styles.dropArea}>
+        {!previewUrl && !isUploadFileLoading && (
+          <div
+            className={getValidClassNames(
+              styles.dropArea,
+              isDragActive && styles.dropAreaActive,
+            )}
+          >
             <input
               type="file"
-              id="coverId"
+              onDrop={handleDrop}
+              onDragEnter={handleDragEnter}
+              onDragLeave={handleDragLeave}
               onChange={handleUploadArticleCover}
               accept={SUPPORTED_FILE_TYPES_STRING}
               className={styles.fileInput}
