5d98d1a57452ce083222a139b6cb3bffc056eb27
drDramen <vvpalchyn@gmail.com>
2023-09-08 22:43:12 +0300

    wr-001: + add TooltipPositionStrategy enum

--- frontend/src/libs/enums/enums.ts
+++ frontend/src/libs/enums/enums.ts
@@ -5,6 +5,7 @@ export { DataStatus } from './data-status.enum.js';
 export { DataTooltipId } from './data-tooltip-id.enum.js';
 export { InputType } from './input-type.enum.js';
 export { TooltipPosition } from './tooltip-position.enum.js';
+export { TooltipPositionStrategy } from './tooltip-position-strategy.enum.js';
 export {
   ApiPath,
   AppEnvironment,
