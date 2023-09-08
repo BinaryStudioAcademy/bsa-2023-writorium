9655ab8b56a7a32c426295f87e3edb7c128108ec
drDramen <vvpalchyn@gmail.com>
2023-09-08 18:31:15 +0300

    wr-95: * change error message

--- backend/src/libs/packages/exceptions/file-size-limit-exceeded-error/file-size-limit-exceeded-error.exception.ts
+++ backend/src/libs/packages/exceptions/file-size-limit-exceeded-error/file-size-limit-exceeded-error.exception.ts
@@ -1,9 +1,10 @@
+import { MAX_FILE_SIZE_MB } from '~/libs/packages/file/file.package.js';
 import { HttpCode, HttpError } from '~/libs/packages/http/http.js';
 
 class FileSizeLimitExceededError extends HttpError {
   public constructor() {
     super({
-      message: 'File size limit exceeded',
+      message: `The selected file exceeds the maximum allowed size of ${MAX_FILE_SIZE_MB} Mb`,
       status: HttpCode.CONTENT_TOO_LARGE,
     });
   }
