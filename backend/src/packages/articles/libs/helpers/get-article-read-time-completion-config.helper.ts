9e1a191c31827d0b0cf0b511cf7e0bd2d2f4a048
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-10 11:17:09 +0300

    wr-193: * set openai default completion temperature to 0

--- backend/src/packages/articles/libs/helpers/get-article-read-time-completion-config.helper.ts
+++ backend/src/packages/articles/libs/helpers/get-article-read-time-completion-config.helper.ts
@@ -1,11 +1,8 @@
 import { type CompletionConfig } from '~/libs/packages/openai/libs/types/completion-config.type.js';
 
-import { DETECT_ARTICLE_GENRE_COMPLETION_TEMPERATURE } from '../constants/constants.js';
-
 const getArticleReadTimeCompletionConfig = (
   text: string,
 ): CompletionConfig => ({
-  temperature: DETECT_ARTICLE_GENRE_COMPLETION_TEMPERATURE,
   prompt: `You will be provided with a text of an article, and your task is to estimate how much time (in minutes) does the regular person need to read it. Provide your output in json format with key 'readTime' and number of minutes as value. Here is the text: ${text}.`,
 });
 
