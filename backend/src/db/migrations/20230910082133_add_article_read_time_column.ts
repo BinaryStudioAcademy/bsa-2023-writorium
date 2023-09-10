df9b4ca5dc929eb56fbfea3810723210c3dbb3ad
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-10 11:26:11 +0300

    wr-193: + migration to add readTime column

---
+++ backend/src/db/migrations/20230910082133_add_article_read_time_column.ts
@@ -0,0 +1,19 @@
+import { type Knex } from 'knex';
+
+const TableName = 'articles' as const;
+
+const ColumnName = 'readTime' as const;
+
+const up = (knex: Knex): Promise<void> => {
+  return knex.schema.alterTable(TableName, (table) => {
+    table.integer(ColumnName).unsigned().nullable();
+  });
+};
+
+const down = (knex: Knex): Promise<void> => {
+  return knex.schema.alterTable(TableName, (table) => {
+    table.dropColumn(ColumnName);
+  });
+};
+
+export { down, up };
