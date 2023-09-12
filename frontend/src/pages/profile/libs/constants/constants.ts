d35940e05bcd436ea26d59d552e20352820ed4ed
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-09-12 09:43:44 +0300

    wr-137: * update activity table formatting

--- frontend/src/pages/profile/libs/constants/constants.ts
+++ frontend/src/pages/profile/libs/constants/constants.ts
@@ -6,7 +6,17 @@ const PROGRESS_MIN = 0;
 const PROGRESS_MAX = 100;
 const NUMBER_OF_ACHIEVEMENTS_TO_DISPLAY = 3;
 
+const DATE_LOCALE = 'en-US';
+const DATE_OPTIONS = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+} as const;
+
 export {
+  DATE_LOCALE,
+  DATE_OPTIONS,
   NUMBER_OF_ACHIEVEMENTS_TO_DISPLAY,
   PROGRESS_MAX,
   PROGRESS_MIN,
