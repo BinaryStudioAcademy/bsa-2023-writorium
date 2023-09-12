08b082121329f989cfcd8d1874bdabf3c702399d
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-09-12 13:17:19 +0300

    wr-137: + helper to make plural or singular forms

--- frontend/src/libs/helpers/get-publication-time/get-publication-time.helper.ts
+++ frontend/src/libs/helpers/get-publication-time/get-publication-time.helper.ts
@@ -1,13 +1,12 @@
 import { type TimeUnit } from '~/libs/enums/enums.js';
+import { makePluralOrSingular } from '~/libs/helpers/helpers.js';
 import { type ValueOf } from '~/libs/types/types.js';
 
 const getPublicationTime = (
   time: number,
   unit: ValueOf<typeof TimeUnit>,
 ): string => {
-  const isPlural = time > 1;
-
-  return `${time} ${unit}${isPlural ? 's' : ''}`;
+  return `${time} ${makePluralOrSingular(unit, time)}`;
 };
 
 export { getPublicationTime };
