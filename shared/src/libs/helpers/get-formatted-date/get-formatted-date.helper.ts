add377459e63e76125844454bef4a76d72584e97
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-09-12 11:53:01 +0300

    wr-137: * get formatted date helper

--- shared/src/libs/helpers/get-formatted-date/get-formatted-date.helper.ts
+++ shared/src/libs/helpers/get-formatted-date/get-formatted-date.helper.ts
@@ -1,4 +1,5 @@
 import { format } from 'date-fns';
+import { enUS } from 'date-fns/locale';
 
 import { type DateFormat } from '~/libs/enums/enums.js';
 import { type ValueOf } from '~/libs/types/types.js';
@@ -6,8 +7,9 @@ import { type ValueOf } from '~/libs/types/types.js';
 const getFormattedDate = (
   date: string,
   dateFormat: ValueOf<typeof DateFormat>,
+  options = { locale: enUS } as { locale: Locale },
 ): string => {
-  return format(new Date(date), dateFormat);
+  return format(new Date(date), dateFormat, options);
 };
 
 export { getFormattedDate };
