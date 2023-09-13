cc4560f3408b26bb3da85a62521fd7ed39908c09
canterbery <wotphantommail@gmail.com>
2023-09-13 13:32:42 +0300

    wr-184: + genres package

---
+++ frontend/src/packages/genres/genres.ts
@@ -0,0 +1,14 @@
+import { config } from '~/libs/packages/config/config.js';
+import { http } from '~/libs/packages/http/http.js';
+import { storage } from '~/libs/packages/storage/storage.js';
+
+import { GenresApi } from './genres-api.package.js';
+
+const genresApi = new GenresApi({
+  baseUrl: config.ENV.API.ORIGIN_URL,
+  storage,
+  http,
+});
+
+export { genresApi };
+export { type GenreGetAllResponseDto } from './libs/types/types.js';
