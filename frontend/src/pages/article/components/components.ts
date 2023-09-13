8c61dc2331edc32301dd7f777eac365c7efd589f
drDramen <vvpalchyn@gmail.com>
2023-09-13 14:56:03 +0300

    wr-151: + implement comment create form

--- frontend/src/pages/article/components/components.ts
+++ frontend/src/pages/article/components/components.ts
@@ -1,3 +1,4 @@
 export { ArticleForm } from './article-form/article-form.js';
 export { ArticleView } from './article-view/article-view.js';
 export { AuthorDetails } from './author-details/author-details.js';
+export { CommentForm } from './comment-form/comment-form.js';
