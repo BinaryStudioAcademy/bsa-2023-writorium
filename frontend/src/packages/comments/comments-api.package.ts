f038596091b795793ab5e6068acacb07b809ad2e
drDramen <vvpalchyn@gmail.com>
2023-09-13 14:12:25 +0300

    wr-151: + implement commentsApi package

---
+++ frontend/src/packages/comments/comments-api.package.ts
@@ -0,0 +1,74 @@
+import { ApiPath, ContentType } from '~/libs/enums/enums.js';
+import { HttpApi } from '~/libs/packages/api/api.js';
+import { type IHttp } from '~/libs/packages/http/http.js';
+import { type IStorage } from '~/libs/packages/storage/storage.js';
+
+import { CommentsApiPath } from './libs/enums/enums.js';
+import {
+  type CommentBaseRequestDto,
+  type CommentGetAllResponseDto,
+  type CommentUpdateRequestDto,
+  type CommentWithRelationsResponseDto,
+} from './libs/types/types.js';
+
+type Constructor = {
+  baseUrl: string;
+  http: IHttp;
+  storage: IStorage;
+};
+
+class CommentsApi extends HttpApi {
+  public constructor({ baseUrl, http, storage }: Constructor) {
+    super({ path: ApiPath.COMMENTS, baseUrl, http, storage });
+  }
+
+  public async fetchAllByArticleId(
+    articleId: number,
+  ): Promise<CommentGetAllResponseDto> {
+    const response = await this.load(
+      this.getFullEndpoint(CommentsApiPath.ROOT, {}),
+      {
+        method: 'GET',
+        contentType: ContentType.JSON,
+        hasAuth: true,
+        query: { articleId },
+      },
+    );
+
+    return await response.json<CommentGetAllResponseDto>();
+  }
+
+  public async create(
+    payload: CommentBaseRequestDto,
+  ): Promise<CommentWithRelationsResponseDto> {
+    const response = await this.load(
+      this.getFullEndpoint(CommentsApiPath.ROOT, {}),
+      {
+        method: 'POST',
+        contentType: ContentType.JSON,
+        payload: JSON.stringify(payload),
+        hasAuth: true,
+      },
+    );
+
+    return await response.json<CommentWithRelationsResponseDto>();
+  }
+
+  public async update(
+    payload: CommentUpdateRequestDto,
+  ): Promise<CommentWithRelationsResponseDto> {
+    const response = await this.load(
+      this.getFullEndpoint(CommentsApiPath.$ID, {}),
+      {
+        method: 'PATCH',
+        contentType: ContentType.JSON,
+        payload: JSON.stringify(payload),
+        hasAuth: true,
+      },
+    );
+
+    return await response.json<CommentWithRelationsResponseDto>();
+  }
+}
+
+export { CommentsApi };
