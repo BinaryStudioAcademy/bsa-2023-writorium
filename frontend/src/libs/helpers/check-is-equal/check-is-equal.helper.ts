f87079dbe53cfa2d6b70e831dd139f66934cd012
canterbery <wotphantommail@gmail.com>
2023-09-14 09:26:51 +0300

    wr-184: * make it arrow function

--- frontend/src/libs/helpers/check-is-equal/check-is-equal.helper.ts
+++ frontend/src/libs/helpers/check-is-equal/check-is-equal.helper.ts
@@ -1,7 +1,7 @@
 import { default as isEqual } from 'lodash.isequal';
 
-function checkIsEqual<T>(value1: T, value2: T): boolean {
+const checkIsEqual = <T>(value1: T, value2: T): boolean => {
   return isEqual(value1, value2);
-}
+};
 
 export { checkIsEqual };
