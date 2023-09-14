e86798c3f744c8db33547e286648f759d9ccc2de
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-14 22:39:46 +0300

    wr-194: * fix styles

--- frontend/src/pages/article/components/article-improvement-suggestions/article-improvement-suggestions.tsx
+++ frontend/src/pages/article/components/article-improvement-suggestions/article-improvement-suggestions.tsx
@@ -1,5 +1,7 @@
-import { IconButton } from '~/libs/components/components.js';
+import { IconButton, Loader } from '~/libs/components/components.js';
+import { DataStatus } from '~/libs/enums/data-status.enum.js';
 import { useAppDispatch, useAppSelector } from '~/libs/hooks/hooks.js';
+import { type ValueOf } from '~/libs/types/types.js';
 import { actions as articlesActions } from '~/slices/articles/articles.js';
 
 import { SuggestionCard } from './libs/components/components.js';
@@ -11,10 +13,12 @@ type Properties = {
 
 const ArticleImprovementSuggestions: React.FC<Properties> = ({ articleId }) => {
   const dispatch = useAppDispatch();
-  const { improvementSuggestions } = useAppSelector(({ articles }) => ({
-    improvementSuggestions: articles.improvementSuggestions,
-    improvementSuggestionsDataStatus: articles.improvementSuggestionsDataStatus,
-  }));
+  const { improvementSuggestions, dataStatus } = useAppSelector(
+    ({ articles }) => ({
+      improvementSuggestions: articles.improvementSuggestions,
+      dataStatus: articles.improvementSuggestionsDataStatus,
+    }),
+  );
 
   const handleSuggestImprovement = (): void => {
     void dispatch(articlesActions.getImprovementSuggestions(articleId));
@@ -23,33 +27,52 @@ const ArticleImprovementSuggestions: React.FC<Properties> = ({ articleId }) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.wrapperInner}>
-        <div className={styles.ctaText}>
-          Enhance your article to make it stand out!
-        </div>
-        <IconButton
-          iconName="autoFix"
-          label="Suggest Improvements"
-          onClick={handleSuggestImprovement}
-          className={styles.suggestImprovementsButton}
-        />
-        <div className={styles.suggestionsListTitle}>
-          <div className={styles.suggestionsCount}>
-            {improvementSuggestions.length}
+        {dataStatus !== DataStatus.PENDING && (
+          <div className={styles.ctaText}>
+            Enhance your article to make it stand out!
           </div>
-          <h3>All suggestions</h3>
-        </div>
-        <ul className={styles.suggestionsList}>
-          {improvementSuggestions.map((item) => {
-            return (
-              <SuggestionCard
-                key={item.title}
-                title={item.title}
-                description={item.description}
-                priority={item.priority}
-              />
-            );
-          })}
-        </ul>
+        )}
+        {(
+          [DataStatus.IDLE, DataStatus.REJECTED] as ValueOf<typeof DataStatus>[]
+        ).includes(dataStatus) && (
+          <IconButton
+            iconName="autoFix"
+            label="Suggest Improvements"
+            onClick={handleSuggestImprovement}
+            className={styles.suggestImprovementsButton}
+          />
+        )}
+        {dataStatus === DataStatus.PENDING && (
+          <div>
+            <Loader isLoading type="dots" />
+            <p className={styles.loadingDescription}>
+              Just a moment! <br /> Our AI is hard at work analyzing your
+              article.
+            </p>
+          </div>
+        )}
+        {dataStatus === DataStatus.FULFILLED && (
+          <>
+            <div className={styles.suggestionsListTitle}>
+              <div className={styles.suggestionsCount}>
+                {improvementSuggestions.length}
+              </div>
+              <h3>All suggestions</h3>
+            </div>
+            <ul className={styles.suggestionsList}>
+              {improvementSuggestions.map((item) => {
+                return (
+                  <SuggestionCard
+                    key={item.title}
+                    title={item.title}
+                    description={item.description}
+                    priority={item.priority}
+                  />
+                );
+              })}
+            </ul>
+          </>
+        )}
       </div>
     </div>
   );
