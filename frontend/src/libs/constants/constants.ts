3311edd03f8a79444e5f8a460b010778d43ee9df
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-09-15 17:47:40 +0300

    wr-232: * rename constant file

--- frontend/src/libs/constants/constants.ts
+++ frontend/src/libs/constants/constants.ts
@@ -1,3 +1,3 @@
-export { PREVIOUS_PAGE_INDEX } from './previous-page-index.js';
+export { PREVIOUS_PAGE_INDEX } from './previous-page-index.constant.js';
 export { RESET_PASSWORD_ROUTE } from './reset-password-route.constant.js';
 export { SHARED_$TOKEN, SUPPORTED_FILE_TYPES } from 'shared/build/index.js';
