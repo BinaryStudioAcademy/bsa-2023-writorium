81ff0982e328e0b0d8225ede95172348ba310930
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2023-09-16 09:44:03 +0300

    wr-200: - unnecessary code

--- frontend/src/libs/enums/data-tooltip-id.enum.ts
+++ frontend/src/libs/enums/data-tooltip-id.enum.ts
@@ -2,7 +2,6 @@ const DataTooltipId = {
   MAIN_TOOLTIP: 'main-tooltip',
   ACHIEVEMENT_TOOLTIP: 'achievement-tooltip',
   PROMPT_TAG_TOOLTIP: 'prompt-tag-tooltip',
-  ARTICLE_ACTIONS_TOOLTIP: 'article-actions-tooltip',
 } as const;
 
 export { DataTooltipId };
