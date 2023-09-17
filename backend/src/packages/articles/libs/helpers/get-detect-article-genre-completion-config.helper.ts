f470477be95c0471470107b0b7cdbfc4ff0b315d
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-17 18:25:16 +0300

    wr-90: * mofify detect genre prompt to handle unknown genre

--- backend/src/packages/articles/libs/helpers/get-detect-article-genre-completion-config.helper.ts
+++ backend/src/packages/articles/libs/helpers/get-detect-article-genre-completion-config.helper.ts
@@ -3,7 +3,7 @@ import { type CompletionConfig } from '~/libs/packages/openai/libs/types/types.j
 const getDetectArticleGenreCompletionConfig = (
   text: string,
 ): CompletionConfig => ({
-  prompt: `You will be provided with a text of an article, and your task is to extract a list of possible genres of that article. Provide your output in json format as an array of objects with keys name and key, where name is a readble name of the genre and key - a short snake cased representation of genre name. Place the most relevant genre at the 0 index. Here is the text: ${text}.`,
+  prompt: `You will be provided with a text of an article, and your task is to extract a list of possible genres of that article. Provide your output in json format as an array of objects with keys name and key, where name is a readble name of the genre and key - a short snake cased representation of genre name. Place the most relevant genre at the 0 index. In case you can't indentify the genre put an object { key: "Unknown", name: "Unknown" } as the first item in resulting array. Here is the text: ${text}.`,
 });
 
 export { getDetectArticleGenreCompletionConfig };
