6d37c6e3c215148a1c20d5f05b72d06e2a50c9f6
Anna Kovalchuk <annazukh@gmail.com>
2023-09-18 09:22:20 +0300

    wr-224: + add title to the list

--- frontend/src/pages/profile/components/user-latest-articles/user-latest-articles.tsx
+++ frontend/src/pages/profile/components/user-latest-articles/user-latest-articles.tsx
@@ -33,22 +33,25 @@ const UserLatestArticles: React.FC<Properties> = ({ className }) => {
 
   return (
     <div className={getValidClassNames(className, styles.wrapper)}>
-      <ol>
-        {articles.map((article) => (
-          <li key={article.id} className={styles.articleTitle}>
-            <Link
-              to={
-                configureString(AppRoute.ARTICLES_$ID, {
-                  id: String(article.id),
-                }) as typeof AppRoute.ARTICLES_$ID
-              }
-              className={styles.articleLink}
-            >
-              {article.title}
-            </Link>
-          </li>
-        ))}
-      </ol>
+      <div>
+        <h3 className={styles.listTitle}>Your latest articles</h3>
+        <ol>
+          {articles.map((article) => (
+            <li key={article.id} className={styles.articleTitle}>
+              <Link
+                to={
+                  configureString(AppRoute.ARTICLES_$ID, {
+                    id: String(article.id),
+                  }) as typeof AppRoute.ARTICLES_$ID
+                }
+                className={styles.articleLink}
+              >
+                {article.title}
+              </Link>
+            </li>
+          ))}
+        </ol>
+      </div>
       <Link to={AppRoute.ARTICLES_MY_ARTICLES} className={styles.showAll}>
         Show all
       </Link>
