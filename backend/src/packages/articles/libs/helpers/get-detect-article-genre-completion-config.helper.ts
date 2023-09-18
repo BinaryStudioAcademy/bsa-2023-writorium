beb2543f07b7265905dd9e953e2e79942995ee0d
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-18 08:27:48 +0300

    wr-90: * fix typo

--- backend/src/packages/articles/libs/helpers/get-detect-article-genre-completion-config.helper.ts
+++ backend/src/packages/articles/libs/helpers/get-detect-article-genre-completion-config.helper.ts
@@ -4,7 +4,7 @@ import { UNKNOWN_GENRE_KEY } from '~/packages/genres/genre.js';
 const getDetectArticleGenreCompletionConfig = (
   text: string,
 ): CompletionConfig => ({
-  prompt: `You will be provided with an html markup of an article, and your task is to extract a list of possible genres of that article. Provide your output in json format as an array of objects with keys name and key, where name is a readble name of the genre and key - a short snake cased representation of genre name. Place the most relevant genre at the 0 index. In case you can't indentify the genre put an object { key: "${UNKNOWN_GENRE_KEY}", name: "Unknown" } as the first item in resulting array. Here is the text: ${text}.`,
+  prompt: `You will be provided with a html markup of an article, and your task is to extract a list of possible genres of that article. Provide your output in json format as an array of objects with keys name and key, where name is a readble name of the genre and key - a short snake cased representation of genre name. Place the most relevant genre at the 0 index. In case you can't indentify the genre put an object { key: "${UNKNOWN_GENRE_KEY}", name: "Unknown" } as the first item in resulting array. Here is the text: ${text}.`,
 });
 
 export { getDetectArticleGenreCompletionConfig };
