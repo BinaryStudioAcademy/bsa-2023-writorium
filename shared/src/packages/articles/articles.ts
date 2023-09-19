7a80f6de95e767b5ba7e7fc142a69a65e4cf55f7
drDramen <vvpalchyn@gmail.com>
2023-09-19 23:56:04 +0300

    wr-223: + add types

--- shared/src/packages/articles/articles.ts
+++ shared/src/packages/articles/articles.ts
@@ -15,6 +15,7 @@ export {
   type ArticleUpdateRequestDto,
   type ArticleUpdateRequestPayload,
   type ArticleWithCommentCountResponseDto,
+  type ArticleWithFollowResponseDto,
   type ArticleWithRelationsType,
   type ReactionResponseDto,
 } from './libs/types/types.js';
