b292ecf2e1cdde0a1e23b8c47539cdc151efe28f
canterbery <wotphantommail@gmail.com>
2023-09-19 19:46:05 +0300

    wr-234: + extension

--- frontend/src/libs/components/text-editor/libs/enums/enums.ts
+++ frontend/src/libs/components/text-editor/libs/enums/enums.ts
@@ -2,4 +2,5 @@ export { HeaderLevel } from './header-level.js';
 export { ListType } from './list-type.js';
 export { TextAlignment } from './text-alignment.js';
 export { TextDecoration } from './text-decoration.js';
+export { TextSize } from './text-size.js';
 export { TextStyle } from './text-style.js';
