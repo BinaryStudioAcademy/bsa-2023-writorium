5bb56e14163c7326f03e467d29981bc37f0f1342
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2023-09-19 20:47:49 +0300

    wr-275: + article views table

---
+++ backend/src/db/migrations/20230919142053_create_article_views_table.ts
@@ -0,0 +1,45 @@
+import { type Knex } from 'knex';
+
+const TableName = {
+  USERS: 'users',
+  ARTICLES: 'articles',
+  ARTICLE_VIEWS: 'article_views',
+} as const;
+
+const ColumnName = {
+  ARTICLE_ID: 'article_id',
+  VIEWED_BY_ID: 'viewed_by_id',
+  ID: 'id',
+  CREATED_AT: 'created_at',
+  UPDATED_AT: 'updated_at',
+};
+
+const up = (knex: Knex): Promise<void> => {
+  return knex.schema.createTable(TableName.ARTICLE_VIEWS, (table) => {
+    table
+      .integer(ColumnName.ARTICLE_ID)
+      .unsigned()
+      .references(ColumnName.ID)
+      .inTable(TableName.ARTICLES);
+    table
+      .integer(ColumnName.VIEWED_BY_ID)
+      .unsigned()
+      .references(ColumnName.ID)
+      .inTable(TableName.USERS);
+    table
+      .dateTime(ColumnName.CREATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .dateTime(ColumnName.UPDATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table.primary([ColumnName.ARTICLE_ID, ColumnName.VIEWED_BY_ID]);
+  });
+};
+
+const down = (knex: Knex): Promise<void> => {
+  return knex.schema.dropTableIfExists(TableName.ARTICLE_VIEWS);
+};
+
+export { down, up };
