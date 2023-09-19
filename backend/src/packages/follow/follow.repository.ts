61d5634a19d89af8603541844731cbc4356707c9
drDramen <vvpalchyn@gmail.com>
2023-09-19 23:59:15 +0300

    wr-223: + add follow package

---
+++ backend/src/packages/follow/follow.repository.ts
@@ -0,0 +1,65 @@
+import { type FollowModel } from './follow.model.js';
+import { type UserFollowResponseDto } from './libs/types/types.js';
+
+class FollowRepository {
+  private followModel: typeof FollowModel;
+
+  public constructor(followModel: typeof FollowModel) {
+    this.followModel = followModel;
+  }
+
+  public async toggleFollowAuthor({
+    userId,
+    authorId,
+  }: {
+    userId: number;
+    authorId: number;
+  }): Promise<FollowModel | FollowModel[]> {
+    const isFollowing = await this.followModel
+      .query()
+      .where({ userId, authorId })
+      .first();
+
+    if (!isFollowing) {
+      return await this.followModel
+        .query()
+        .insert({ userId, authorId })
+        .returning('*');
+    }
+
+    return await this.followModel
+      .query()
+      .delete()
+      .where({ userId, authorId })
+      .returning('*');
+  }
+
+  public async getAuthorFollowersCountAndStatus({
+    userId,
+    authorId,
+  }: {
+    userId?: number;
+    authorId: number;
+  }): Promise<UserFollowResponseDto> {
+    return await this.followModel.transaction(async (trx) => {
+      const followersCount = await this.followModel
+        .query(trx)
+        .where({ authorId })
+        .count()
+        .first()
+        .castTo<{ count: string }>();
+
+      const isFollowed = userId
+        ? await this.followModel.query(trx).where({ userId, authorId }).first()
+        : false;
+
+      return {
+        authorId,
+        followersCount: Number.parseInt(followersCount?.count, 10),
+        isFollowed: !!isFollowed,
+      };
+    });
+  }
+}
+
+export { FollowRepository };
