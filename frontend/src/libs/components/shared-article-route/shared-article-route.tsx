87f066940eac038f89dfc05478eebe3dfc78fe5d
drDramen <vvpalchyn@gmail.com>
2023-09-21 22:44:17 +0300

    wr-287: * use configureString helper

--- frontend/src/libs/components/shared-article-route/shared-article-route.tsx
+++ frontend/src/libs/components/shared-article-route/shared-article-route.tsx
@@ -1,5 +1,6 @@
 import { Loader, Navigate } from '~/libs/components/components.js';
 import { AppRoute, DataStatus } from '~/libs/enums/enums.js';
+import { configureString } from '~/libs/helpers/helpers.js';
 import {
   useAppDispatch,
   useAppSelector,
@@ -49,7 +50,14 @@ const SharedArticleRoute: React.FC<Properties> = ({
 
     return (
       <Loader isLoading={isLoading} hasOverlay type="circular">
-        <Navigate to={`${AppRoute.ARTICLES}/${articleId}`} replace />
+        <Navigate
+          to={
+            configureString(AppRoute.ARTICLES_$ID, {
+              id: String(articleId),
+            }) as typeof AppRoute.ARTICLES_$ID
+          }
+          replace
+        />
       </Loader>
     );
   }
