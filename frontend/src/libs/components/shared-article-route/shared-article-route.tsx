43b08e800c32f8f1d68334f50af4f129f25beb87
drDramen <vvpalchyn@gmail.com>
2023-09-21 19:35:36 +0300

    wr-287: + implement SharedArticleRoute

---
+++ frontend/src/libs/components/shared-article-route/shared-article-route.tsx
@@ -0,0 +1,60 @@
+import { Loader, Navigate } from '~/libs/components/components.js';
+import { AppRoute, DataStatus } from '~/libs/enums/enums.js';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useEffect,
+  useParams,
+} from '~/libs/hooks/hooks.js';
+import { type ValueOf } from '~/libs/types/types.js';
+import { actions as articlesActions } from '~/slices/articles/articles.js';
+
+type Properties = {
+  children: React.ReactNode;
+  redirectPath?: ValueOf<typeof AppRoute>;
+};
+
+const SharedArticleRoute: React.FC<Properties> = ({
+  children,
+  redirectPath = AppRoute.ARTICLES,
+}) => {
+  const dispatch = useAppDispatch();
+  const { user, articleId, articleIdByTokenDataStatus } = useAppSelector(
+    ({ auth, articles: { articleIdByToken, articleIdByTokenDataStatus } }) => ({
+      user: auth.user,
+      articleId: articleIdByToken,
+      articleIdByTokenDataStatus: articleIdByTokenDataStatus,
+    }),
+  );
+  const { token } = useParams();
+  const hasUser = Boolean(user);
+
+  useEffect(() => {
+    if (hasUser && token) {
+      void dispatch(articlesActions.geArticleIdByToken({ token }));
+    }
+
+    return () => void dispatch(articlesActions.resetArticleIdByToken());
+  }, [dispatch, hasUser, token]);
+
+  if (hasUser) {
+    const isLoading = !(
+      articleIdByTokenDataStatus === DataStatus.FULFILLED ||
+      articleIdByTokenDataStatus === DataStatus.REJECTED
+    );
+
+    if (!articleId && !isLoading) {
+      return <Navigate to={redirectPath} />;
+    }
+
+    return (
+      <Loader isLoading={isLoading} hasOverlay type="circular">
+        <Navigate to={`${AppRoute.ARTICLES}/${articleId}`} replace />
+      </Loader>
+    );
+  }
+
+  return children;
+};
+
+export { SharedArticleRoute };
