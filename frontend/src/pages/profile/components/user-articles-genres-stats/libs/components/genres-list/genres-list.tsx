cae5e508ca82024bd5bb9572eb2b14d5453d6e53
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-09-21 15:48:24 +0300

    wr-265: + genres list component

---
+++ frontend/src/pages/profile/components/user-articles-genres-stats/libs/components/genres-list/genres-list.tsx
@@ -0,0 +1,28 @@
+import { type UserArticlesGenreStatsItem } from '~/packages/users/users.js';
+
+import { GENRES_CHART_COLORS } from '../../constants/constants.js';
+import styles from './styles.module.scss';
+
+type Properties = {
+  chartData: UserArticlesGenreStatsItem[];
+};
+
+const GenresList: React.FC<Properties> = ({ chartData }) => {
+  return (
+    <ul className={styles.list}>
+      {chartData.map((data, index) => (
+        <li
+          key={data.key}
+          className={styles.listItem}
+          style={{
+            color: GENRES_CHART_COLORS?.[index % GENRES_CHART_COLORS.length],
+          }}
+        >
+          <span className={styles.itemName}>{data.name}</span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export { GenresList };
