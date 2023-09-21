95c905e85be21a1a27deae2ba53070922c31067c
canterbery <wotphantommail@gmail.com>
2023-09-21 14:28:08 +0300

    wr-282: + constants

---
+++ frontend/src/pages/profile/components/user-articles-genres-stats/libs/constants/article-status-select-options.constant.ts
@@ -0,0 +1,8 @@
+import { type SelectOption } from '~/libs/types/types.js';
+
+const articleStatusOptions: SelectOption[] = [
+  { label: 'Published', value: 'published' },
+  { label: 'Draft', value: 'draft' },
+];
+
+export { articleStatusOptions };
