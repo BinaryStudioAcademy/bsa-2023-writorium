c6508b2a0146d916e5080039d14465dbcb0a926e
Oleg Hnoievets <olegg7548@gmail.com>
2023-09-22 17:16:46 +0300

    wr-203: - shortens in migration to record achievements

--- backend/src/db/migrations/20230920200620_record_achieved_achievements_in_users_achievements_table.ts
+++ backend/src/db/migrations/20230920200620_record_achieved_achievements_in_users_achievements_table.ts
@@ -9,25 +9,51 @@ const TableName = {
 
 const up = (knex: Knex): Promise<void> => {
   return knex.raw(`
-    WITH UsersCounts AS (
-      SELECT t.user_id, t.reference_table, COUNT(*)
+    WITH user_counts AS (
+      SELECT
+        grouped_reference_tables_counts.user_id,
+        grouped_reference_tables_counts.reference_table,
+        COUNT(*)
       FROM (
-        SELECT c.user_id, c.id, a.reference_table
-        FROM ${TableName.COMMENTS} c
-        JOIN ${TableName.ACHIEVEMENTS} a ON a.reference_table = '${TableName.COMMENTS}'
-        GROUP BY c.user_id, c.id, a.reference_table
+        SELECT
+          ${TableName.COMMENTS}.user_id,
+          ${TableName.COMMENTS}.id,
+          ${TableName.ACHIEVEMENTS}.reference_table
+        FROM ${TableName.COMMENTS}
+        JOIN ${TableName.ACHIEVEMENTS}
+        ON ${TableName.ACHIEVEMENTS}.reference_table = '${TableName.COMMENTS}'
+        GROUP BY
+          ${TableName.COMMENTS}.user_id,
+          ${TableName.COMMENTS}.id,
+          ${TableName.ACHIEVEMENTS}.reference_table
         UNION ALL
-        SELECT ar.user_id, ar.id, a.reference_table
-        FROM ${TableName.ARTICLES} ar
-        JOIN ${TableName.ACHIEVEMENTS} a ON a.reference_table = '${TableName.ARTICLES}'
-        GROUP BY ar.user_id, ar.id, a.reference_table
-      ) t
-      GROUP BY t.user_id, t.reference_table
+        SELECT
+          ${TableName.ARTICLES}.user_id,
+          ${TableName.ARTICLES}.id,
+          ${TableName.ACHIEVEMENTS}.reference_table
+        FROM ${TableName.ARTICLES}
+        JOIN ${TableName.ACHIEVEMENTS}
+        ON ${TableName.ACHIEVEMENTS}.reference_table = '${TableName.ARTICLES}'
+        GROUP BY
+          ${TableName.ARTICLES}.user_id,
+          ${TableName.ARTICLES}.id,
+          ${TableName.ACHIEVEMENTS}.reference_table
+      ) grouped_reference_tables_counts
+      GROUP BY
+        grouped_reference_tables_counts.user_id,
+        grouped_reference_tables_counts.reference_table
     )
     INSERT INTO ${TableName.USERS_ACHIEVEMENTS} (user_id, achievement_id)
-    SELECT uc.user_id, a.id AS achievement_id
-    FROM UsersCounts uc, ${TableName.ACHIEVEMENTS} a
-    WHERE uc.reference_table = a.reference_table AND uc.count >= a.breakpoint;
+    SELECT
+      user_counts.user_id,
+      ${TableName.ACHIEVEMENTS}.id AS achievement_id
+    FROM
+      user_counts,
+      ${TableName.ACHIEVEMENTS}
+    WHERE
+      user_counts.reference_table = ${TableName.ACHIEVEMENTS}.reference_table
+      AND
+      user_counts.count >= ${TableName.ACHIEVEMENTS}.breakpoint;
   `);
 };
 
