90aac6175233f0c61f5a2ce031514eda0568f280
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-24 10:51:59 +0300

    wr-245: - write link changes

--- frontend/src/libs/components/header/header.tsx
+++ frontend/src/libs/components/header/header.tsx
@@ -9,7 +9,6 @@ import { AppRoute } from '~/libs/enums/enums.js';
 import { getFullName } from '~/libs/helpers/helpers.js';
 import { useAppDispatch, useCallback } from '~/libs/hooks/hooks.js';
 import { type UserAuthResponseDto } from '~/packages/users/users.js';
-import { actions as appActions } from '~/slices/app/app.js';
 import { actions as authActions } from '~/slices/auth/auth.js';
 
 import styles from './styles.module.scss';
@@ -25,10 +24,6 @@ const Header: React.FC<Properties> = ({ user }) => {
     void dispatch(authActions.logout());
   }, [dispatch]);
 
-  const handleWriteButtonClick = (): void => {
-    dispatch(appActions.navigate(AppRoute.CREATE_ARTICLE));
-  };
-
   return (
     <>
       {user && (
@@ -37,11 +32,9 @@ const Header: React.FC<Properties> = ({ user }) => {
             <Icon iconName="writoriumLogo" className={styles.logo} />
           </Link>
           <div className={styles.rightSide}>
-            <Button
-              label="Write"
-              className={styles.writeButton}
-              onClick={handleWriteButtonClick}
-            />
+            <Link to={AppRoute.CREATE_ARTICLE} className={styles.writeLink}>
+              Write
+            </Link>
             <Popover
               content={
                 <>
