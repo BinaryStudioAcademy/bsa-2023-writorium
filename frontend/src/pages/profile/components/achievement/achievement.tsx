674c3bd1f667ba158a4e36541f3b2f5adeeb94aa
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-24 13:43:29 +0300

    wr-293: * achivement item redesign

--- frontend/src/pages/profile/components/achievement/achievement.tsx
+++ frontend/src/pages/profile/components/achievement/achievement.tsx
@@ -1,8 +1,7 @@
-import { type FC } from 'react';
-
+import { ReactComponent as ArticlesAchievementIcon } from '~/assets/img/articles-achievement.svg';
+import { ReactComponent as CommentsAchievementIcon } from '~/assets/img/comments-achievement.svg';
 import { getValidClassNames } from '~/libs/helpers/helpers.js';
 import { type AchievementWithProgressResponseDto } from '~/packages/achievements/achievements.js';
-import { getProgressStyleClass } from '~/pages/profile/libs/helpers/helpers.js';
 
 import styles from './styles.module.scss';
 
@@ -11,19 +10,47 @@ type Properties = {
   className?: string;
 };
 
-const Achievement: FC<Properties> = ({ achievement, className }) => {
-  const { name, progress } = achievement;
-  const statusStyleClass = getProgressStyleClass(progress);
+const Achievement: React.FC<Properties> = ({ achievement }) => {
+  const { name, referenceTable, progress } = achievement;
+  const radius = 45;
+  const circumference = Math.PI * 2 * radius;
+  const offset = (progress / 100) * circumference;
+
+  const achievementEntityClassName: string | undefined = {
+    'comments': styles.comments,
+    'articles': styles.articles,
+  }[referenceTable];
+
+  const AchievementImage: React.FC<React.SVGProps<SVGSVGElement>> | undefined =
+    {
+      'comments': CommentsAchievementIcon,
+      'articles': ArticlesAchievementIcon,
+    }[referenceTable];
 
   return (
-    <div
-      className={getValidClassNames(
-        styles.achievementItem,
-        className,
-        styles[statusStyleClass],
-      )}
-    >
-      <h4>{name}</h4>
+    <div className={styles.achievementWrapper}>
+      <div
+        className={getValidClassNames(
+          styles.achievementBadge,
+          achievementEntityClassName,
+          !progress && styles.notStarted,
+        )}
+      >
+        <svg viewBox="0 0 100 100" className={styles.progressWrapper}>
+          <circle cx="50" cy="50" r={radius} className={styles.progressTrack} />
+          <circle
+            cx="50"
+            cy="50"
+            r={radius}
+            className={styles.progress}
+            strokeDasharray={`${offset} ${circumference}`}
+          />
+        </svg>
+        <div className={styles.achievementImage}>
+          {AchievementImage && <AchievementImage />}
+        </div>
+      </div>
+      <h4 className={styles.name}>{name}</h4>
     </div>
   );
 };
