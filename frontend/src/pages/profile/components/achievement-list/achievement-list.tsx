10ab6411f45605735fc69468bf26ac30d2cc5a91
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-09-28 10:19:59 +0300

    wr-388: * achievement description

--- frontend/src/pages/profile/components/achievement-list/achievement-list.tsx
+++ frontend/src/pages/profile/components/achievement-list/achievement-list.tsx
@@ -1,16 +1,17 @@
 import { BlockWithTooltip } from '~/libs/components/components.js';
 import { DataTooltipId } from '~/libs/enums/enums.js';
 import { getValidClassNames } from '~/libs/helpers/helpers.js';
-import { type AchievementWithProgressResponseDto } from '~/packages/achievements/achievements.js';
+import { type AchievementWithParsedDescription } from '~/packages/achievements/achievements.js';
 
 import {
   Achievement,
   AchievementTooltipContent,
 } from '../../components/components.js';
+import { getAchievementDescriptionByProgress } from '../../libs/helpers/helpers.js';
 import styles from './styles.module.scss';
 
 type Properties = {
-  achievements: AchievementWithProgressResponseDto[];
+  achievements: AchievementWithParsedDescription[];
   hasToShowTooltip?: boolean;
   className?: string;
   classNameAchievement?: string;
@@ -23,31 +24,32 @@ const AchievementList: React.FC<Properties> = ({
   classNameAchievement,
   classNameBadge,
   hasToShowTooltip = false,
-}) => {
-  return (
-    <ul className={getValidClassNames(className, styles.achievementList)}>
-      {achievements.map((achievement) => (
-        <li key={achievement.id} className={styles.achievementListItem}>
-          <BlockWithTooltip
-            tooltipContent={
-              <AchievementTooltipContent
-                description={achievement.description}
-                progress={achievement.progress}
-              />
-            }
-            tooltipId={DataTooltipId.ACHIEVEMENT_TOOLTIP}
-            hasToShowTooltip={hasToShowTooltip}
-          >
-            <Achievement
-              achievement={achievement}
-              className={classNameAchievement}
-              classNameBadge={classNameBadge}
+}) => (
+  <ul className={getValidClassNames(className, styles.achievementList)}>
+    {achievements.map((achievement) => (
+      <li key={achievement.id} className={styles.achievementListItem}>
+        <BlockWithTooltip
+          tooltipContent={
+            <AchievementTooltipContent
+              description={getAchievementDescriptionByProgress(
+                achievement.progress,
+                achievement.description,
+              )}
+              progress={achievement.progress}
             />
-          </BlockWithTooltip>
-        </li>
-      ))}
-    </ul>
-  );
-};
+          }
+          tooltipId={DataTooltipId.ACHIEVEMENT_TOOLTIP}
+          hasToShowTooltip={hasToShowTooltip}
+        >
+          <Achievement
+            achievement={achievement}
+            className={classNameAchievement}
+            classNameBadge={classNameBadge}
+          />
+        </BlockWithTooltip>
+      </li>
+    ))}
+  </ul>
+);
 
 export { AchievementList };
